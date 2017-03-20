a5037f2972561cc406ccd0ec1cc9b3180cb6d597
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-03-20 11:17:03 +0900

    submit A.cpp to 'A - はじめてのあっとこーだー（Welcome to AtCoder）' (practice) [C++14 (GCC 5.4.1)]

---
+++ practice/A.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main () {
+  int a;
+  int b, c;
+  string s;
+  cin >> a >> b >> c;
+  cin >> s;
+  cout << a + b + c << " " << s << endl;
+}
