f1d5426ca736107d6f1face9dfadcca902195e80
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-04-01 21:07:40 +0900

    submit A.cpp to 'A - AtCoder Group Contest' (agc012) [C++14 (GCC 5.4.1)]

---
+++ 0401_AGC012/A.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int N;
+ll a[100010];
+
+int main () {
+  cin >> N;
+  N = 3 * N;
+  for (auto i = 0; i < N; ++i) {
+    cin >> a[i];
+  }
+  sort(a, a+N);
+  reverse(a, a+N);
+  ll ans = 0;
+  int M = N/3;
+  int cnt = 0;
+  for (auto i = 0; i < N; ++i) {
+    if (i%2 == 1) {
+      ans += a[i];
+      ++cnt;
+      if (M == cnt) break;
+    }
+  }
+  cout << ans << endl;
+}
