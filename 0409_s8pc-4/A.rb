4d0b0e8d3b69cbee88ad4e11c89b469f9f1a5bc3
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-04-09 20:41:15 +0900

    tried A.rb to 'A'

---
+++ 0409_s8pc-4/A.rb
@@ -0,0 +1,36 @@
+def f_or_l(str, text)
+  a = str.gsub(/?/, 'a')
+  z = str.gsub(/?/, 'z')
+  if z < text
+    return 2
+  elsif a > text
+    return 1
+  else
+    return 0
+  end
+end
+
+n = gets.to_i
+s = []
+for i in 0..n
+  s << gets.chomp
+end
+t = gets.chomp
+
+f = 0
+l = 0
+
+s.each{|str|
+  res = f_or_l(str, t)
+  if res == 1
+    f += 1
+  elsif res == 2
+    l += 1
+  end
+}
+
+ans = []
+for i in f..(n - l)
+  ans << i
+end
+puts ans.join(" ")
