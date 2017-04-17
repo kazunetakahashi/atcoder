ac2d922e0aace4c3d569c0ebb4f53b5ebd340be0
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-04-17 16:52:08 +0900

    move.rb

--- move.rb
+++ move.rb
@@ -1,7 +1,7 @@
-folder_name = "past_201510_201609"
+folder_name = "2017"
 
 Dir.open('.').each {|dir|
-  if dir[0] == '0' || dir[0] == '1'
+  if dir[0] == '0'
     puts dir
     system("git mv #{dir}/ #{folder_name}/")
   end
