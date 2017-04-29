0b900f1eb2b77cd5bad5ac8d6d59aab8874ea11c
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-04-29 21:18:11 +0900

    tried D.cpp to 'D'

---
+++ 2017/0429_ARC073/D.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int N;
+vector<int> V[4];
+vector<int> S[4];
+int W;
+int w[110];
+int v[110];
+int K;
+
+int main () {
+  cin >> N >> W;
+  for (auto i = 0; i < N; ++i) {
+    cin >> w[i] >> v[i];
+  }
+  K = w[0];
+  for (auto i = 0; i < N; ++i) {
+    V[w[i] - K].push_back(v[i]);
+  }
+  for (auto i = 0; i < 4; ++i) {
+    int sum = 0;
+    sort(V[i].begin(), V[i].end());
+    reverse(V[i].begin(), V[i].end());
+    S[i].push_back(0);
+    for (auto x : V[i]) {
+      sum += x;
+      S[i].push_back(x);
+    }
+  }
+  int ans = 0;
+  for (auto i = 0; i < (int)S[0].size(); ++i) {
+    for (auto j = 0; j < (int)S[1].size(); ++j) {
+      for (auto k = 0; k < (int)S[2].size(); ++k) {
+        int weight = (i+j+k) * K + j + 2 * k;
+        int l = (W - weight)/(K+3);
+        ans = max(ans, S[0][i] + S[1][j] + S[2][k] + S[3][l]);
+      }
+    }
+  }
+  cout << ans << endl;
+}
