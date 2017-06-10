a8259993d1bc32a8db55a90d5115a8c91a9d011a
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-06-10 21:12:33 +0900

    tried D.rb to 'D'

---
+++ 2017/0610_ABC064/D.rb
@@ -0,0 +1,29 @@
+n = gets.to_i
+s = gets.chomp
+need = 0
+right = 0
+s.each{|c|
+  if c == '('
+    right -= 1
+  else
+    right += 1
+  end
+  need = [need, right].max
+}
+need.times {
+  s = '(' + s
+}
+left = 0
+right = 0
+s.each{|c|
+  if c == '('
+    left += 1
+  else
+    right += 1
+  end
+}
+(left - right).times{
+  s = s + ')'
+}
+puts s
+
