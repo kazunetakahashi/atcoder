9f1e6d5bdbeaaf0349694bba19e240fec355cba9
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-07-01 21:08:30 +0900

    submit C.cpp to 'C - pushpush' (arc077) [C++14 (GCC 5.4.1)]

---
+++ 2017/0701_ARC077/C.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main () {
+  int n;
+  cin >> n;
+  int a[200010];
+  for (auto i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+  deque<int> Q;
+  bool is_back = true;
+  for (auto i = 0; i < n; ++i) {
+    if (is_back) {
+      Q.push_back(a[i]);
+    } else {
+      Q.push_front(a[i]);
+    }
+    is_back = !is_back;
+  }
+  vector<int> V;
+  for (auto x : Q) {
+    V.push_back(x);
+  }
+  if (!is_back) {
+    reverse(V.begin(), V.end());
+  }
+  for (auto i = 0; i < n; ++i) {
+    cout << V[i];
+    if (i < n-1) {
+      cout << " ";
+    } else {
+      cout << endl;
+    }
+  }
+}
