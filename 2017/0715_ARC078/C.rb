35881e004a2fd9e1873daf711d370e1e0d9fea53
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-07-15 22:28:40 +0900

    submit C.rb to 'C - Splitting Pile' (arc078) [Ruby (2.3.3)]

--- 2017/0715_ARC078/C.rb
+++ 2017/0715_ARC078/C.rb
@@ -4,15 +4,25 @@ a = gets.chomp.split(" ").map{|i| i.to_i}.sort_by{|i|
 }
 x = 0
 y = 0
+cx = 0
+cy = 0
 a.each{|i|
   if (x < y && i > 0)
     x += i
+    cx += 1
   elsif (x < y && i <= 0)
     y += i
+    cy += 1
   elsif (x >= y && i > 0)
     y += i
+    cy += 1
   else
     x += i
+    cx += 1
   end
 }
-puts (x-y).abs
+if cx == 0 || cy == 0
+  puts (x-y).abs + 2 * a.last.abs
+else
+  puts (x-y).abs
+end
