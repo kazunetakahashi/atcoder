748ce736b1fd2bb7e8cd93a431a133e820726838
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-07-15 23:33:12 +0900

    tried C.rb to 'C'

--- 2017/0715_ARC078/C.rb
+++ 2017/0715_ARC078/C.rb
@@ -1,28 +1,10 @@
 n = gets.to_i
-a = gets.chomp.split(" ").map{|i| i.to_i}.sort_by{|i|
-  -1 * i.abs
-}
-x = 0
-y = 0
-cx = 0
-cy = 0
-a.each{|i|
-  if (x < y && i > 0)
-    x += i
-    cx += 1
-  elsif (x < y && i <= 0)
-    y += i
-    cy += 1
-  elsif (x >= y && i > 0)
-    y += i
-    cy += 1
-  else
-    x += i
-    cx += 1
-  end
-}
-if cx == 0 || cy == 0
-  puts (x-y).abs + 2 * a.last.abs
-else
-  puts (x-y).abs
+a = gets.chomp.split(" ")
+sum = a.inject(:+)
+now = 0
+ans = sum
+for i in 1..n-1
+  now += a[i]
 end
+ans = [ans, (sum-now - now).abs].min
+puts ans
