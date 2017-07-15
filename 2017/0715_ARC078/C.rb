e9a2e9c3e3d680125e5259e243025ebde87677df
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-07-15 23:35:17 +0900

    tried C.rb to 'C'

--- 2017/0715_ARC078/C.rb
+++ 2017/0715_ARC078/C.rb
@@ -3,7 +3,8 @@ a = gets.chomp.split(" ").map{|i| i.to_i}
 sum = a.inject(:+)
 now = a[0]
 ans = sum
-for i in 1...n-1
+for i in 1...(n-1)
+  puts i
   now += a[i]
   ans = [ans, (sum-now - now).abs].min
 end
