964672fa5a089f1454881e6f0d0b883556611a13
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-07-23 22:09:58 +0900

    tried B.cpp to 'B'

---
+++ 2017/0723_agc018/B.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int N, M;
+int A[310][310];
+queue<int> Q[310];
+
+int main () {
+  cin >> N >> M;
+  for (auto i = 0; i < N; ++i) {
+    for (auto j = 0; j < M; ++j) {
+      cin >> A[i][j];
+      A[i][j]--;
+      Q[i].push(A[i][j]);
+    }
+  }
+  int ans = N;
+  while (true) {
+    for (auto i = 0; i < N; ++i) {
+      if (Q[i].empty()) {
+        break;
+      }
+      int cnt[310];
+      fill(cnt, cnt+M, 0);
+      for (auto i = 0; i < N; ++i) {
+        cnt[Q[i].front()]++;
+      }
+      int t = 0;
+      int max_ind = 100000;
+      for (auto i = 0; i < M; ++i) {
+        if (t < cnt[i]) {
+          t = cnt[i];
+          max_ind = i;
+        }
+      }
+      assert(max_ind < 100000);
+      ans = min(ans, t);
+      for (auto i = 0; i < N; ++i) {
+        if (Q[i].front() == max_ind) {
+          Q[i].pop();
+        }
+      }
+    }
+  }
+  cout << ans << endl;
+}
