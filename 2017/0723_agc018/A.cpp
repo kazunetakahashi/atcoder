7cc61d04a96fe957d0936c3d0e02adbfe925cfb0
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-07-23 21:08:44 +0900

    submit A.cpp to 'A - Getting Difference' (agc018) [C++14 (GCC 5.4.1)]

---
+++ 2017/0723_agc018/A.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+const int MAX_SIZE = 1000010;
+const long long MOD = 1000000007;
+
+long long inv[MAX_SIZE];
+long long fact[MAX_SIZE];
+long long factinv[MAX_SIZE];
+
+void init() {
+  inv[1] = 1;
+  for (int i=2; i<MAX_SIZE; i++) {
+    inv[i] = ((MOD - inv[MOD%i]) * (MOD/i))%MOD;
+  }
+  fact[0] = factinv[0] = 1;
+  for (int i=1; i<MAX_SIZE; i++) {
+    fact[i] = (i * fact[i-1])%MOD;
+    factinv[i] = (inv[i] * factinv[i-1])%MOD;
+  }
+}
+
+long long C(int n, int k) {
+  if (n >=0 && k >= 0 && n-k >= 0) {
+    return ((fact[n] * factinv[k])%MOD * factinv[n-k])%MOD;
+  }
+  return 0;
+}
+
+long long power(long long x, long long n) {
+  if (n == 0) {
+    return 1;
+  } else if (n%2 == 1) {
+    return (x * power(x, n-1)) % MOD;
+  } else {
+    long long half = power(x, n/2);
+    return (half * half) % MOD;
+  }
+}
+
+long long gcm(long long a, long long b) {
+  if (a < b) {
+    return gcm(b, a);
+  }
+  if (b == 0) return a;
+  return gcm(b, a%b);
+}
+
+ll N, K;
+ll A[100010];
+
+int main() {
+  init();
+  cin >> N >> K;
+  for (auto i = 0; i < N; ++i) {
+    cin >> A[i];
+  }
+  ll g = A[0];
+  for (auto i = 1; i < N; ++i) {
+    g = gcm(g, A[i]);
+  }
+  for (auto i = 0; i < N; ++i) {
+    if (A[i]-K >= 0 && (A[i]-K) % g == 0) {
+      cout << "POSSIBLE" << endl;
+      return 0;
+    }
+  }
+  cout << "IMPOSSIBLE" << endl;
+}
