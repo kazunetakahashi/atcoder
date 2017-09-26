9fd2b9fe53c1233bd87a59fe6df1e7f9cc508762
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-09-26 23:53:21 +0900

    submit C.cpp to 'C - Make a Rectangle' (arc081) [C++14 (GCC 5.4.1)]

---
+++ 2017/0926_ARC081/C.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int N;
+ll A[100010];
+
+int main () {
+  cin >> N;
+  for (auto i = 0; i < N; ++i) {
+    cin >> A[i];
+  }
+  sort(A, A+N);
+  reverse(A, A+N);
+  vector<ll> V;
+  for (auto i = 0; i < N-1; ++i) {
+    if (A[i] == A[i+1]) {
+      V.push_back(A[i]);
+      i++;
+    }
+  }
+  if ((int)V.size() < 2) {
+    cout << 0 << endl;
+  } else {
+    cout << V[0] * V[1] << endl;
+  }
+}
