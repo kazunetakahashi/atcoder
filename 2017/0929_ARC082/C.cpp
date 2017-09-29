10d5bdd09b544f68652de0d537c073da12ee1896
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-09-29 23:24:13 +0900

    tried C.cpp to 'C'

---
+++ 2017/0929_ARC082/C.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int N;
+int a[100010];
+int cnt[100010];
+
+int main () {
+  cin >> N;
+  for (auto i = 0; i < N; ++i) {
+    cin >> a[i];
+  }
+  fill(cnt, cnt+100010, 0);
+  for (auto i = 0; i < N; ++i) {
+    cnt[a[i]]++;
+  }
+  int ans = 0;
+  for (auto i = 1; i < 100004; ++i) {
+    ans = max(ans, cnt[i-1] + cnt[i] + cnt[i-1]);
+  }
+  cout << ans << endl;
+}
