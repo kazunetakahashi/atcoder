8f9641f2068aced6d4297f45912f0234794a3c1c
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-09-29 23:30:34 +0900

    submit D.cpp to 'D - Derangement' (arc082) [C++14 (GCC 5.4.1)]

---
+++ 2017/0929_ARC082/D.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int N;
+int p[100010];
+
+int main () {
+  cin >> N;
+  for (auto i = 0; i < N; ++i) {
+    cin >> p[i];
+    p[i]--;
+  }
+  bool same[100010];
+  for (auto i = 0; i < N; ++i) {
+    same[i] = (p[i] == i);
+  }
+  same[N] = false;
+  int ans = 0;
+  for (auto i = 0; i < N; ++i) {
+    if (same[i] && same[i+1]) {
+      ++ans;
+      same[i] = false;
+      same[i+1] = false;
+    } else if (same[i]) {
+      ++ans;
+      same[i] = false;
+    }
+  }
+  cout << ans << endl;
+}
