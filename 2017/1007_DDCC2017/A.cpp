2b183308cff769f43eeb0c5d574249b0c065d9f9
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-10-07 21:01:22 +0900

    submit A.cpp to 'A - DDCC型文字列' (ddcc2017-qual) [C++14 (GCC 5.4.1)]

---
+++ 2017/1007_DDCC2017/A.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main () {
+  string S;
+  cin >> S;
+  if (S[0] == S[1] && S[1] != S[2] && S[2] == S[3]) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;    
+  }
+}
