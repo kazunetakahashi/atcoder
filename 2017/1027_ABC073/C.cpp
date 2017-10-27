3ba4f14f4d53664122243597a5498cdae5f81d69
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-10-27 18:53:33 +0900

    submit C.cpp to 'C - Write and Erase' (abc073) [C++14 (GCC 5.4.1)]

---
+++ 2017/1027_ABC073/C.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+set<int> S;
+
+int main () {
+  int N;
+  cin >> N;
+  int A[100010];
+  for (auto i = 0; i < N; ++i) {
+    cin >> A[i];
+  }
+  for (auto i = 0; i < N; ++i) {
+    auto it = S.find(A[i]);
+    if (it == S.end()) {
+      S.insert(A[i]);
+    } else {
+      S.erase(it);
+    }
+  }
+  cout << S.size() << endl;
+}
