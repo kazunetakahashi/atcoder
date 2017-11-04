f94121c419e85724dc80b8fa89018f06225d7e05
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-04 22:39:22 +0900

    tried D.rb to 'D'

---
+++ 2017/1104_ARC084/D.rb
@@ -0,0 +1,36 @@
+k = gets.to_i
+
+k /= k.gcd(10)
+
+def sum(k)
+  ans = 0
+  k.to_s.chars{|i|
+    ans += i.to_i
+  }
+  return ans
+end
+
+for i in 1...1000
+  str = ""
+  i.times{
+    str += "1"    
+  }
+  if str.to_i % k == 0
+    puts sum(k)
+    exit
+  end
+end
+
+if k%9 == 0
+  puts 9
+  exit
+end
+
+if k%3 == 0
+  puts 3
+  exit
+end
+
+puts 2
+
+
