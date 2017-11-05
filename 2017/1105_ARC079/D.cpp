d392af8da65f7bd1025fa3dae6934e2b31af1667
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-05 22:05:28 +0900

    tried D.cpp to 'D'

--- 2017/1105_ARC079/D.cpp
+++ 2017/1105_ARC079/D.cpp
@@ -34,6 +34,9 @@ typedef long long ll;
 int main () {
   ll K;
   cin >> K;
-  cout << 2 << endl;
-  cout << (K+2)/2 << " " << (K+2) - (K+2)/2 << endl;
+  if (K%2 != 0) {
+    cout << 2 << endl;
+    cout << (K+2)/2 << " " << (K+2) - (K+2)/2 << endl;    
+  }
+  return 1;
 }
