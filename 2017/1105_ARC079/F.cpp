79d2748bea385c4627bc9973ccf9a5f8b00d3c37
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-06 00:11:09 +0900

    tried F.cpp to 'F'

--- 2017/1105_ARC079/F.cpp
+++ 2017/1105_ARC079/F.cpp
@@ -109,14 +109,13 @@ int main () {
     for (auto i = 0; i < N; ++i) {
       if (isloop[i] && i != now) calc(i);
     }
-    /*
     for (auto i = 0; i < N; ++i) {
       if (isloop[i]) {
         cerr << "calc(" << i << ") = " << calc(i) << endl;
       }
     }
-    */
     memo[now] = -1;
+    cerr << calc(now) << " " << z << endl;
     if (calc(now) == z) {
       cout << "POSSIBLE" << endl;
       return 0;
