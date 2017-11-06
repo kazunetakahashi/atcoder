74dbef5c3e5cfe85f44dc12246eadd248c13dee1
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-06 14:12:24 +0900

    submit B.cpp to 'B - ss' (abc066) [C++14 (GCC 5.4.1)]

---
+++ 2017/1106_ABC066/B.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main () {
+  string S;
+  cin >> S;
+  int N = S.size();
+  for (auto i = 1; i < N; ++i) {
+    string T = S.substr(0, N-i);
+    int M = T.size();
+    if (M % 2 == 0) {
+      string X = T.substr(0, M/2);
+      string Y = T.substr(M/2, M/2);
+      if (X == Y) {
+        cout << M << endl;
+        return 0;
+      }
+    }
+  }
+}
