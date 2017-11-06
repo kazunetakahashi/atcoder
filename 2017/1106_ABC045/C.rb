e597fa98d198135fcf3d7593e89c0e91e1f30ca0
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-06 17:28:56 +0900

    tried C.rb to 'C'

---
+++ 2017/1106_ABC045/C.rb
@@ -0,0 +1,15 @@
+
+@sum = 0
+
+def dfs(s, pos)
+  if s.size() == pos
+    @sum += eval(s)
+  else
+    dfs(s[0...pos] + '+' + s[pos...s.size()], pos+2)
+    dfs(s[0...pos] + s[pos...s.size()], pos+1)
+  end
+end
+
+dfs(gets.chomp, 0)
+
+puts @sum
