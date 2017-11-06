85e64ea4e5619d77f5f5fe87a090389ea968f9eb
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-06 15:42:49 +0900

    submit C.cpp to 'C - 背の順' (abc041) [C++14 (GCC 5.4.1)]

---
+++ 2017/1106_ABC041/C.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+typedef tuple<int, int> student;
+
+int main () {
+  vector<student> V;
+  int N;
+  cin >> N;
+  for (auto i = 0; i < N; ++i) {
+    int a;
+    cin >> a;
+    V.push_back(student(a, i+1));
+  }
+  sort(V.begin(), V.end());
+  reverse(V.begin(), V.end());
+  for (auto x : V) {
+    cout << get<1>(x) << endl;
+  }
+}
+
