0aab809889e9051523f3d524fa4e604a65fe1fb4
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-06 13:58:36 +0900

    tried B.cpp to 'B'

---
+++ 2017/1106_ABC067/B.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main () {
+  int N, K;
+  cin >> N >> K;
+  int l[51];
+  for (auto i = 0; i < N; ++i) {
+    cin >> l[i];
+  }
+  sort(l, l+50);
+  reverse(l, l+50);
+  int ans = 0;
+  for (auto i = 0; i < K; ++i) {
+    ans += l[i];
+  }
+  cout << ans << endl;
+}
