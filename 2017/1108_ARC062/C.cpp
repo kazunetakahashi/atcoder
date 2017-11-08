ae51cd7452dbf78828759e9fbbf6a5835da20680
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-08 20:40:52 +0900

    submit C.cpp to 'C - AtCoDeerくんと選挙速報 / AtCoDeer and Election Report' (arc062) [C++14 (GCC 5.4.1)]

---
+++ 2017/1108_ARC062/C.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main () {
+  int N;
+  cin >> N;
+  ll T[1010];
+  ll A[1010];
+  for (auto i = 0; i < N; ++i) {
+    cin >> T[i] >> A[i];
+  }
+
+  ll X = 1, Y = 1;
+  for (auto i = 0; i < N; ++i) {
+    ll k = max((X+T[i]-1)/T[i], (Y+A[i]-1)/A[i]);
+    X = T[i] * k;
+    Y = A[i] * k;
+  }
+  cout << X + Y << endl;
+}
