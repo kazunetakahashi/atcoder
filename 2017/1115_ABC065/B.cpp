86b4b1a2552cacb7d0dcc371f52b60f9a9a07109
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-15 06:32:18 +0900

    tried B.cpp to 'B'

---
+++ 2017/1115_ABC065/B.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main () {
+  int N;
+  int a[100010];
+  cin >> N;
+  for (auto i = 0; i < N; ++i) {
+    cin >> a[i];
+    a[i]--;
+  }
+  int now = 0;
+  int ans = 0;
+  while (ans < 200010) {
+    now = a[now];
+    ans++;
+  }
+  if (ans > 100010) {
+    cout << -1 << endl;
+  } else {
+    cout << ans << endl;
+  }
+}
