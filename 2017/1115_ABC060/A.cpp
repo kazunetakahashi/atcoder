0063cb863e00dd313cd1ff7acd456fd89d42ea4a
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-15 06:19:54 +0900

    submit A.cpp to 'A - Shiritori' (abc060) [C++14 (GCC 5.4.1)]

---
+++ 2017/1115_ABC060/A.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main () {
+  string A, B, C;
+  cin >> A >> B >> C;
+  if (A[A.size()-1] == B[0] && B[B.size()-1] == C[0]) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+}
