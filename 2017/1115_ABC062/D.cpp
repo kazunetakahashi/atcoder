e02c3647fbf1f63e9eef0ec3ea15cfad5d58703e
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-15 07:29:11 +0900

    tried D.cpp to 'D'

---
+++ 2017/1115_ABC062/D.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+const ll infty = 10000000000000007;
+
+int N;
+ll a[300010];
+ll plusscore[300010];
+ll minusscore[300010];
+
+int main () {
+  cin >> N;
+  for (auto i = 0; i < N; ++i) {
+    cin >> a[i];
+  }
+  fill(plusscore, plusscore+300010, -infty);
+  fill(minusscore, minusscore+300010, infty);
+  priority_queue<ll, vector<ll>, greater<ll> > Q;
+  ll sum = 0;
+  for (auto i = 0; i < 2*N; ++i) {
+    Q.push(a[i]);
+    sum += a[i];
+    while ((int)Q.size() > N) {
+      sum -= Q.top();
+      Q.pop();      
+    }
+    plusscore[i] = sum;
+  }
+  priority_queue<ll> QQ;
+  sum = 0;
+  for (auto i = 3*N-1; i >= N; --i) {
+    QQ.push(a[i]);
+    sum += a[i];
+    while ((int)QQ.size() > N) {
+      sum -= QQ.top();
+      Q.pop();      
+    }
+    minusscore[i] = sum;
+  }
+  ll ans = -infty * 3;
+  for (auto i = N; i < 2*N; ++i) {
+    ans = min(ans, plusscore[i] - minusscore[i+1]);
+  }
+  cout << ans << endl;
+}
