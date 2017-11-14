e4361d779cb80f5658cab1066347e7e23ed8ccbf
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-15 06:29:09 +0900

    submit A.cpp to 'A - Expired?' (abc065) [C++14 (GCC 5.4.1)]

---
+++ 2017/1115_ABC065/A.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main () {
+  int X, A, B;
+  cin >> X >> A >> B;
+  if (A >= B) {
+    cout << "delicious" << endl;
+  } else if (A+X >= B) {
+    cout << "safe" << endl;
+  } else {
+    cout << "dangerous" << endl;
+  }
+}
