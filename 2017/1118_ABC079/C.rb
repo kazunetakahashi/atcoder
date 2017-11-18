bdfaa7fbe3b3c76db91aa9c002ddd6ccd729ccab
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-18 21:07:11 +0900

    tried C.rb to 'C'

---
+++ 2017/1118_ABC079/C.rb
@@ -0,0 +1,14 @@
+n = gets.chomp
+
+def dfs(s, i)
+  if i == 3
+    if eval(s) == 7
+      puts s + "=7"
+      exit
+    end
+  end
+  dfs(s[0...2*i+1] + '+' + s[2*i+1...s.size], i+1)
+  dfs(s[0...2*i+1] + '-' + s[2*i+1...s.size], i+1)
+end
+
+dfs(n, 0)
