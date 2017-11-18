a21fc7b511c6bffd7f4bd97dd57b95ceaef8476c
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-18 21:13:51 +0900

    submit D.cpp to 'D - Wall' (abc079) [C++14 (GCC 5.4.1)]

---
+++ 2017/1118_ABC079/D.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main () {
+  int H, W;
+  cin >> H >> W;
+  ll c[10][10];
+  for (auto i = 0; i < 10; ++i) {
+    for (auto j = 0; j < 10; ++j) {
+      cin >> c[i][j];
+    }
+  }
+  vector<int> A;
+  for (auto i = 0; i < H; ++i) {
+    for (auto j = 0; j < W; ++j) {
+      int x;
+      cin >> x;
+      if (x != -1) A.push_back(x);
+    }
+  }
+  for (auto k = 0; k < 10; ++k) {
+    for (auto i = 0; i < 10; ++i) {
+      for (auto j = 0; j < 10; ++j) {
+        c[i][j] = min(c[i][j], c[i][k] + c[k][j]);
+      }
+    }
+  }
+  ll ans = 0;
+  for (auto x : A) {
+    ans = ans + c[x][1];
+  }
+  cout << ans << endl;
+}
