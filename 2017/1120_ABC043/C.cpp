bd28d7d793db75ad34d48ef189688e6927831b47
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-20 22:03:23 +0900

    submit C.cpp to 'C - いっしょ / Be Together' (abc043) [C++14 (GCC 5.4.1)]

---
+++ 2017/1120_ABC043/C.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+const ll infty = 1000000007;
+
+int main () {
+  int N;
+  cin >> N;
+  ll a[110];
+  for (auto i = 0; i < N; ++i) {
+    cin >> a[i];
+  }
+  ll ans = infty;
+  for (auto i = -100; i < 101; ++i) {
+    ll t = 0;
+    for (auto j = 0; j < N; ++j) {
+      ll sa = (a[j] - i);
+      t += sa * sa;
+    }
+    ans = min(t, ans);
+  }
+  cout << ans << endl;
+}
