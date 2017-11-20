080eb535af239701073e0a1ec09a4d3133f6318b
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-20 22:11:25 +0900

    tried D.cpp to 'D'

---
+++ 2017/1120_ABC043/D.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main () {
+  string S;
+  cin >> S;
+  int N = S.size();
+  for (auto i = 0; i < N-1; ++i) {
+    if (S[i] == S[i+1]) {
+      cout << i+1 << " " << i+2 << endl;
+      return 0;
+    }
+  }
+  for (auto i = 0; i < N-2; ++i) {
+    if (S[i] == S[i+2]) {
+      cout << i+1 << " " << i+3 << endl;
+      return 0;
+    }
+  }
+  cout << "-1 -1" << endl;
+}
