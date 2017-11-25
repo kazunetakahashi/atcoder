5f082931b3cb1029d6185bcd9c39bdbe81b3f74e
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-11-25 12:46:34 +0900

    submit B.cpp to 'B - Palindrome-phobia' (cf17-final-open) [C++14 (GCC 5.4.1)]

---
+++ 2017/1125_code-fes-2017-final/B.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main () {
+  string S;
+  cin >> S;
+  int cnt[3];
+  fill(cnt, cnt+3, 0);
+  for (auto x : S) {
+    cnt[x - 'a']++;
+  }
+  sort(cnt, cnt+3);
+  if (abs(cnt[0] - cnt[2]) < 2) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;    
+  }
+}
