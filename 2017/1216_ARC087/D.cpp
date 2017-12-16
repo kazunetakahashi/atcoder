6e68da6b5df24cf0bf92dd9ff324a80f288e9cf6
Kazune Takahashi <kazunetakahashi@gmail.com>
2017-12-16 21:12:38 +0900

    submit D.cpp to 'D - FT Robot' (arc087) [C++14 (GCC 5.4.1)]

---
+++ 2017/1216_ARC087/D.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main () {
+  string S;
+  cin >> S;
+  int x[2];
+  cin >> x[0] >> x[1];
+  vector<int> V[2];
+  bool isx = 0;
+  int cnt = 0;
+  for (auto e : S) {
+    if (e == 'F') cnt++;
+    else {
+      if (cnt > 0) {
+        V[isx].push_back(cnt);
+      }
+      cnt = 0;
+      isx = !isx;
+    }
+  }
+  if (cnt > 0) {
+    V[isx].push_back(cnt);
+  }
+  for (auto k = 0; k < 2; ++k) {
+    int sum = 0;
+    for (auto e : V[k]) {
+      sum += e;
+    }
+    if ((x[k] + sum) %2 != 0) {
+      cout << "No" << endl;
+      return 0;
+    }
+    int mokuhyo = (x[k] + sum)/2;
+    bool dp[10000];
+    fill(dp, dp+10000, false);
+    dp[0] = true;
+    for (auto e : V[k]) {
+      for (auto i = 9999; i >= 0; --i) {
+        if (dp[i] && i + e < 10000) {
+          dp[i+e] = true;
+        }
+      }
+    }
+    if (!dp[mokuhyo]) {
+      cout << "No" << endl;
+      return 0;      
+    }
+  }
+  cout << "Yes" << endl;
+}
