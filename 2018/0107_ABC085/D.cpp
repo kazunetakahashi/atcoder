01dd89626ff40404f601224662cebb9d78974330
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-01-07 21:11:43 +0900

    submit D.cpp to 'D - Katana Thrower' (abc085) [C++14 (GCC 5.4.1)]

---
+++ 2018/0107_ABC085/D.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <iomanip> // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple> // get<n>(xxx)
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set> // S.insert(M);
+// if (S.find(key) != S.end()) { }
+// for (auto it=S.begin(); it != S.end(); it++) { }
+// auto it = S.lower_bound(M);
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib> // atoi(xxx)
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+// insert #if<tab> by my emacs. #if DEBUG == 1 ... #end
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main () {
+  int N;
+  int H;
+  cin >> N >> H;
+  int a[100010], b[100010];
+  for (auto i = 0; i < N; ++i) {
+    cin >> a[i] >> b[i];
+  }
+  int maxi = 0;
+  for (auto i = 0; i < N; ++i) {
+    maxi = max(maxi, a[i]);
+  }
+  sort(b, b+N);
+  reverse(b, b+N);
+  int cnt = 0;
+  for (auto i = 0; i < N; ++i) {
+    if (b[i] > maxi) {
+      H -= b[i];
+      cnt++;      
+    } else {
+      break;
+    }
+    if (H <= 0) {
+      cout << cnt << endl;
+      return 0;
+    }
+  }
+  cnt += (H + maxi - 1) / maxi;
+  cout << cnt << endl;
+}
