de3751f4e49b1bfbb5fd4bd8a1c114241c910bda
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-01-15 00:46:35 +0900

    tried D.cpp to 'D'

--- 2018/0114_AGC020/D.cpp
+++ 2018/0114_AGC020/D.cpp
@@ -47,48 +47,63 @@ string repb(int n) {
   return rep("B", n);
 }
 
-string hanten(string S) {
-  string ans = "";
-  for (auto x : S) {
-    if (x == 'A') {
-      ans += "B";
+ll calc_K(ll A, ll B) {
+  ll ub = 500000010;
+  ll lb = 0;
+  while (ub - lb > 1) {
+    ll t = (ub+lb)/2;
+    if (A <= (B+1) * t && B <= (A+1) * t) {
+      ub = t;
     } else {
-      ans += "A";
+      lb = t;
     }
   }
-  return ans;
+  return ub;
 }
 
-string f(int A, int B) {
-  if (A == 1) {
-    return repb(B/2) + "A" + repb(B - B/2);
-  }
-  if (A > B) {
-    string x = f(B, A);
-    reverse(x.begin(), x.end());
-    return hanten(x);
-  }
-  if (B < A * (A+1)) {
-    int k = B/A;
-    int l = B%A;
-    if (l <= k) {
-      string ans = repb(l);
-      string bu = "A" + repb(k);
-      ans += rep(bu, A);
-      return ans;
+string f(int A, int B, int C, int D) {
+  ll K = calc_K(A, B);
+  ll ub = A+B;
+  ll lb = -1;
+  while (ub - lb > 1) {
+    ll t = (ub+lb)/2;
+    ll b = t/(K+1);
+    ll a = t - b;
+    ll rema = A - a;
+    ll remb = B - b;
+    if (remb <= (rema+1) * K) {
+      ub = t;
     } else {
-      string bu1 = "A" + repb(k);
-      string bu2 = "A" + repb(k+1);
-      return rep(bu1, A-l) + rep(bu2, l);
+      lb = t;
     }
-  } else {
-    int k = B/(A+1);
-    int l = B%(A+1);
-    string bu1 = "A" + repb(k);
-    string bu2 = "A" + repb(k+1);
-    return repb(k) + rep(bu1, A-l) + rep(bu2, l);    
   }
-  assert(false);
+  ll L = ub;
+  if (D <= L) {
+    int back = (C/(K+1)) * (K+1);
+    C -= back;
+    D -= back;
+    string X = rep(repa(K) + "B", 100);
+    return X.substr(C-1, D-C+1);
+  } else if (C > L) {
+    C = A+B+1-C;
+    D = A+B+1-D;
+    int back = (C/(K+1)) * (K+1);
+    C -= back;
+    D -= back;    
+    string X = rep(repb(K) + "A", 100);
+    string Y = X.substr(D-1, D-C+1);
+    reverse(Y.begin(), Y.end());
+    return Y;
+  } else {
+    int rema = L%(K+1);
+    int remb = (A+B-L)%(K+1);
+    string X = repa(rema) + repb(remb);
+    X = rep(repa(K) + "B", 100) + X + rep("A" + repb(K), 100);
+    int back = L - (100 * (K+1) + rema);
+    C -= back;
+    D -= back;
+    return X.substr(C-1, D-C+1);    
+  }
 }
 
 int main () {
@@ -99,7 +114,7 @@ int main () {
     cin >> A[i] >> B[i] >> C[i] >> D[i];
   }
   for (auto i = 0; i < Q; ++i) {
-    string X = f(A[i], B[i]);
-    cout << X.substr(C[i]-1, D[i]-C[i]+1) << endl;
+    string X = f(A[i], B[i], C[i], D[i]);
+    cout << X << endl;
   }
 }
