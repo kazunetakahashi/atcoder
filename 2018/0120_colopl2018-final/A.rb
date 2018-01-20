5119e96e2bd72e027caf782af35cb71665a5eded
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-01-20 13:18:56 +0900

    tried A.rb to 'A'

--- 2018/0120_colopl2018-final/A.rb
+++ 2018/0120_colopl2018-final/A.rb
@@ -1,13 +1,37 @@
 n = gets.to_i
-s = gets.chomp
-now = 0
-ans = 0
-s.chars{|c|
-  if c == 'A'
-    now += 1
-    ans += now
-  else
-    now = 0
+s = []
+s << gets.chomp
+s << s+s
+s << s+s+s
+ans = [0, 0, 0]
+
+isa = true
+s[0].chars{|c|
+  if c == 'B'
+    isa = false
+    break
   end
 }
-puts ans
+
+if isa
+  m = s.size * n
+  puts (m * (m+1))/2
+  exit
+end
+
+for i in 0...3
+  now = 0
+  s[i].chars{|c|
+    if c == 'A'
+      now += 1
+      ans[i] += now
+    else
+      now = 0
+    end
+  }  
+end
+
+k = ans[1] - ans[0]
+l = ans[0]
+
+puts k * (n-1) + l
