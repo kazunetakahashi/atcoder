aa7f4543d365a628d8f69e1940f9cc213fc104d5
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-01-27 21:01:50 +0900

    submit A.cpp to 'A - SoundHound' (soundhound2018) [C++14 (GCC 5.4.1)]

--- 2018/0127_soundhound2018/A.cpp
+++ 2018/0127_soundhound2018/A.cpp
@@ -34,7 +34,7 @@ typedef long long ll;
 int main () {
   string X, Y;
   cin >> X >> Y;
-  if (X[0] == 'S' && X[1] == 'H') {
+  if (X[0] == 'S' && Y[0] == 'H') {
     cout << "YES" << endl;
   } else {
     cout << "NO" << endl;
