296efe458b399df0f8d0f386bc65913974980cfe
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-02-27 15:39:09 +0900

    tried E.cpp to 'E'

--- 2017/1210_ARC086/E.cpp
+++ 2017/1210_ARC086/E.cpp
@@ -116,6 +116,7 @@ vector<deque<vector<ll>>>::iterator make(int v)
       (*it)[j][0] = zero;
       (*it)[j][1] = one;
       (*it)[j][2] = two;
+      tit->clear();
     }
   }
   for (auto i = 0; i < len; i++)
