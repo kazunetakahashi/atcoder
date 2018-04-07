69a1ac6ce66025fb99d80b373f286501e699863a
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-04-07 21:02:09 +0900

    tried C.cpp to 'C'

---
+++ 2018/0407_ARC094/C.cpp
@@ -0,0 +1,45 @@
+/**
+ * File    : C.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2018-4-7 20:58:00
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main()
+{
+  int A[3];
+  cin >> A[0] >> A[1] >> A[2];
+  sort(A, A + 3);
+  int ans = A[2] - A[1];
+  ans += (A[1] - A[0] + 1) % 2;
+  cout << ans << endl;
+}
