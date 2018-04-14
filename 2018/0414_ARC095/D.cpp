44993a6cd86772ff7f6752147bbbbb28748b8613
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-04-14 21:18:01 +0900

    tried D.cpp to 'D'

---
+++ 2018/0414_ARC095/D.cpp
@@ -0,0 +1,56 @@
+/**
+ * File    : D.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2018-4-14 21:15:37
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int N;
+int a[100010];
+
+int main()
+{
+  cin >> N;
+  for (auto i = 0; i < N; i++)
+  {
+    cin >> a[i];
+  }
+  int n = *max_element(a, a + N);
+  int k = a[0];
+  for (auto i = 0; i < N; i++)
+  {
+    if (abs(n/2-a[i]) - abs(n/2-k)) {
+      k = a[i];
+    }
+  }
+  cout << n << " " << k << endl;
+}
