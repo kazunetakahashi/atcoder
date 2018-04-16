113ec89186eb661fdd97ed00cd74f5756e471dba
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-04-17 01:12:15 +0900

    submit E.cpp to 'E - Symmetric Grid' (arc095) [C++14 (GCC 5.4.1)]

--- 2018/0414_ARC095/E.cpp
+++ 2018/0414_ARC095/E.cpp
@@ -24,7 +24,7 @@
 #include <cstdlib>
 using namespace std;
 
-#define DEBUG 1 // change 0 -> 1 if we need debug.
+#define DEBUG 0 // change 0 -> 1 if we need debug.
 
 typedef long long ll;
 
@@ -195,5 +195,5 @@ int main()
       return 0;
     }
   }
-  cerr << "NO" << endl;
+  cout << "NO" << endl;
 }
