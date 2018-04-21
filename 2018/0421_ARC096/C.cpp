3a5b72bf841589e3d4e041aead960710e13be3da
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-04-21 21:06:01 +0900

    tried C.cpp to 'C'

---
+++ 2018/0421_ARC096/C.cpp
@@ -0,0 +1,59 @@
+/**
+ * File    : C.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2018-4-21 20:42:39
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+ll A, B, C, X, Y;
+
+int main()
+{
+  cin >> A >> B >> C >> X >> Y;
+  if (A + B <= 2 * C) {
+    cout << A * X + B * Y << endl;
+    return 0;
+  }
+  ll Z = 2 * min(A, B);
+  X -= Z;
+  Y -= Z;
+  if (A > 2 * C) {
+    Z += 2 * X;
+    X = 0;
+  }
+  if (B > 2 * C) {
+    Z += 2 * Y;
+    Y = 0;
+  }
+  cout << A * X + B * Y + C * Z << endl;
+  return 0;
+}
