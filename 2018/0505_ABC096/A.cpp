aa17a7fc1b4804802822ca7feb802d727c2cd314
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-05-05 21:01:12 +0900

    submit A.cpp to 'A - Day of Takahashi' (abc096) [C++14 (GCC 5.4.1)]

---
+++ 2018/0505_ABC096/A.cpp
@@ -0,0 +1,49 @@
+/**
+ * File    : A.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2018-5-5 20:57:41
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main()
+{
+  int a, b;
+  cin >> a >> b;
+  if (a <= b)
+  {
+    cout << a << endl;
+  }
+  else
+  {
+    cout << a - 1 << endl;
+  }
+}
