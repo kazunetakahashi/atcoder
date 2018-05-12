86f3e981948c4023fd249ede365cbb71cc4ec8c3
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-05-12 22:43:45 +0900

    tried F.cpp to 'F'

--- 2018/0512_ARC097/F.cpp
+++ 2018/0512_ARC097/F.cpp
@@ -58,7 +58,7 @@ int dfs(int v)
     else
     {
       parent[e] = v;
-      X.push_back(dfs(v));
+      X.push_back(dfs(e));
     }
   }
   if ((int)X.size() == 0)
@@ -172,7 +172,7 @@ int main()
       need[i] = true;
     }
   }
-  cerr << cost << endl;
+  // cerr << cost << endl;
   for (auto i = 0; i < N; i++)
   {
     if (!del[i] && S[i] == 'W')
