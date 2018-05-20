b1d4dd2e50f1f1ffc389623bcc6fe732ec9c3c7e
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-05-20 21:03:58 +0900

    submit A.cpp to 'A - Fairness' (agc024) [C++14 (GCC 5.4.1)]

---
+++ 2018/0520_AGC024/A.cpp
@@ -0,0 +1,54 @@
+/**
+ * File    : A.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2018-5-20 20:37:22
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <chrono> // std::chrono::system_clock::time_point start_time, end_time;
+// start = std::chrono::system_clock::now();
+// double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main()
+{
+  ll A[3];
+  cin >> A[0] >> A[1] >> A[2];
+  ll K;
+  cin >> K;
+  if (K % 2 == 0)
+  {
+    cout << A[0] - A[1] << endl;
+  }
+  else
+  {
+    cout << A[1] - A[0] << endl;
+  }
+}
