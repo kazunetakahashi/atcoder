052f5057806a1d2f1c47606575b2bb0b6b114f56
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-05-26 23:00:40 +0900

    submit E.cpp to 'E - Range Minimum Queries' (arc098) [C++14 (GCC 5.4.1)]

--- 2018/0526_ARC098/E.cpp
+++ 2018/0526_ARC098/E.cpp
@@ -135,19 +135,20 @@ int main()
       }
       right[j] = t;
     }
+    /*
     for (auto j = 0; j < N; j++)
     {
       cerr << j << " : " << left[j] << " " << right[j] << endl;
     }
+    */
     BIT bit(N + 1);
     int cnt[2010];
     for (auto j = 0; j <= i; j++)
     {
       k = V[j].second;
-      cerr << "k = " << k << endl;
-      bit.add(k);
-      cnt[k] = bit.sum(left[k] + 1, right[k]);
-      cerr << "cnt[" << k << "] = " << cnt[k] << endl;
+      bit.add(k + 1);
+      cnt[k] = bit.sum(left[k] + 2, right[k] + 1);
+      // cerr << "cnt[" << k << "] = " << cnt[k] << endl;
     }
     int sel = 0;
     for (auto j = i; j >= 0; j--)
