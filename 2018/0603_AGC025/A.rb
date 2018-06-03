c3f5dee9e7f4f7d84840b3ec8a8a50a68405d7a1
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-06-03 21:01:41 +0900

    tried A.rb to 'A'

---
+++ 2018/0603_AGC025/A.rb
@@ -0,0 +1,23 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 2018-6-3 21:00:30
+# Powered by Visual Studio Code
+#
+
+n = gets.to_i
+ans = 0
+
+for a in 1...n
+  b = n = a
+  sum = 0
+  a.to_s.chars{|c|
+    sum += c.to_i
+  }
+  b.to_s.chars{|c|
+    sum += c.to_i
+  }
+  ans = [ans, sum].max
+end
+
+puts ans
