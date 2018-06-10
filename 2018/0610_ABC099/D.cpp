57dc133f20e5a74853ac80cabcf4b07da6a89d9a
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-06-10 21:17:55 +0900

    submit D.cpp to 'D - Good Grid' (abc099) [C++14 (GCC 5.4.1)]

---
+++ 2018/0610_ABC099/D.cpp
@@ -0,0 +1,97 @@
+/**
+ * File    : D.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2018-6-10 21:11:33
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <random> // random_device rd; mt19937 mt(rd());
+#include <chrono> // std::chrono::system_clock::time_point start_time, end_time;
+// start = std::chrono::system_clock::now();
+// double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int N, C;
+ll D[40][40];
+ll c[510][510];
+
+int main()
+{
+  cin >> N >> C;
+  for (auto i = 0; i < C; i++)
+  {
+    for (auto j = 0; j < C; j++)
+    {
+      cin >> D[i][j];
+    }
+  }
+  for (auto i = 0; i < N; i++)
+  {
+    for (auto j = 0; j < N; j++)
+    {
+      cin >> c[i][j];
+      c[i][j]--;
+    }
+  }
+  ll ans = 100000009907;
+  for (auto i = 0; i < C; i++)
+  {
+    for (auto j = 0; j < C; j++)
+    {
+      if (i == j)
+        continue;
+      for (auto k = 0; k < C; k++)
+      {
+        if (i == k || j == k)
+          continue;
+        ll sum = 0;
+        for (auto x = 0; x < N; x++)
+        {
+          for (auto y = 0; y < N; y++)
+          {
+            if ((x + y) % 3 == 0)
+            {
+              sum += D[c[x][y]][i];
+            }
+            else if ((x + y) % 3 == 1)
+            {
+              sum += D[c[x][y]][j];
+            }
+            else
+            {
+              sum += D[c[x][y]][k];
+            }
+          }
+        }
+        ans = min(ans, sum);
+      }
+    }
+  }
+  cout << ans << endl;
+}
