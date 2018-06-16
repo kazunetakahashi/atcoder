3ff8d15d27b6b4c77c3b3351de13fd89955fa8f4
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-06-16 21:01:24 +0900

    tried A.rb to 'A'

---
+++ 2018/0616_ABC100/A.rb
@@ -0,0 +1,17 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 2018-6-16 20:47:32
+# Powered by Visual Studio Code
+#
+
+# a = gets.chomp.split(" ").map{|i| i.to_i}
+# s = gets.chomp
+a = gets.to_i
+b = gets.to_i
+
+if a <= 8 && b <= 8
+  puts "Yay!"
+else
+  puts ":("
+end
