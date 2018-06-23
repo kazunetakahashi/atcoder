6d16d2e120acc69b49a4ecb9b7c3f16a511d136e
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-06-24 01:15:29 +0900

    tried D.rb to 'D'

--- 2018/0623_ARC099/D.rb
+++ 2018/0623_ARC099/D.rb
@@ -11,7 +11,7 @@ a = []
 
 for i in 0...200
   for n in 1...10
-    x = n * 10 ** i + 10 ** i - 1
+    x = n * (10 ** i) + (10 ** i) - 1
     if x > 0
       a << x
     end
@@ -21,7 +21,27 @@ for i in 0...200
   end
 end
 
-for i in 0...k
-  puts a[i]
+# for i in 0...k
+#   puts a[i]
+# end
+
+def f(num)
+  s = 0
+  num.to_s.chars{|c|
+    s += c.to_i
+  }
+  return num / s.to_f
 end
 
+res = 0
+
+for i in 0...k
+  if f(a[i]) >= res
+    puts "f(#{a[i]}) = #{f(a[i])}"
+    res = f(a[i])
+  else
+    puts "WA"
+    puts "f(#{a[i]}) = #{f(a[i])}"
+    exit
+  end
+end
