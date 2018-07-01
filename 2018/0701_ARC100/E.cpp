6dfb804ebeb9f41734002e55c5fd58fa5e000986
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-07-01 22:18:04 +0900

    submit E.cpp to 'E - Or Plus Max' (arc100) [C++14 (GCC 5.4.1)]

--- 2018/0701_ARC100/E.cpp
+++ 2018/0701_ARC100/E.cpp
@@ -73,7 +73,7 @@ void make_S(int i)
 int main()
 {
   cin >> N;
-  for (auto i = 0; i < N; i++)
+  for (auto i = 0; i < (1 << N); i++)
   {
     cin >> A[i];
   }
@@ -81,11 +81,11 @@ int main()
   {
     make_S(i);
     auto it = S[i].begin();
-    cerr << "i = " << i << endl;
-    cerr << "(" << -get<0>(*it) << ", " << get<1>(*it) << ")" << endl;
+    // cerr << "i = " << i << endl;
+    // cerr << "(" << -get<0>(*it) << ", " << get<1>(*it) << ")" << endl;
     DP2[i] = -(get<0>(*it));
     it++;
-    cerr << "(" << -get<0>(*it) << ", " << get<1>(*it) << ")" << endl;
+    // cerr << "(" << -get<0>(*it) << ", " << get<1>(*it) << ")" << endl;
     DP2[i] += -(get<0>(*it));
   }
   // flush
