724546b1f002a3bea28ca78bada2e18ad6c4e47c
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-07-15 15:18:52 +0900

    tried A.rb to 'A'

---
+++ 2018/0714_AGC026/A.rb
@@ -0,0 +1,30 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 2018-7-15 15:12:00
+# Powered by Visual Studio Code
+#
+
+n = gets.to_i
+a = gets.chomp.split(" ").map{|i| i.to_i}
+
+ary = []
+while !a.empty
+  for i in 0...a.size()
+    if a[i] != a[i+1]
+      ary << a[0..i]
+      a = a[i+1...-1]
+      break
+    end
+    ary << a
+    a = []
+  end
+end
+
+ans = 0
+
+ary.each{|it|
+  ans += it.size / 2
+}
+
+puts ans
