9fba58123f370173ddbdc40f5a740c2b7b78b6b6
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-08-05 21:01:24 +0900

    submit A.rb to 'A - Rated for Me' (abc104) [Ruby (2.3.3)]

---
+++ 2018/0805_ABC104/A.rb
@@ -0,0 +1,16 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 2018-8-5 20:58:22
+# Powered by Visual Studio Code
+#
+
+r = gets.to_i
+
+if r < 1200
+  puts "ABC"
+elsif r < 2800
+  puts "ARC"
+else
+  puts "AGC"
+end
