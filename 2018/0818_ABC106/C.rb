e51b37767525cd776b8029c7246dfbe4a8905fd0
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-08-18 21:08:04 +0900

    tried C.rb to 'C'

---
+++ 2018/0818_ABC106/C.rb
@@ -0,0 +1,23 @@
+#
+# File    : C.rb
+# Author  : Kazune Takahashi
+# Created : 8/18/2018, 9:05:13 PM
+# Powered by Visual Studio Code
+#
+
+s = gets.chomp
+k = gets.to_i
+
+if s[0] != "1"
+  puts s[0]
+else
+  for i in 0...(min(k, s.size())
+    if s[i] != "1"
+      puts s[i]
+      exit
+    end
+  end
+  puts "1"
+end
+
+
