a0cf635d56b06896dac08d2b9eb04bf66fae74ae
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-09-30 15:42:00 +0900

    tried E.cpp to 'E'

--- 2018/0930_KUPC2018/E.cpp
+++ 2018/0930_KUPC2018/E.cpp
@@ -166,7 +166,7 @@ int main()
   c[N - 1] = 1;
   for (int i = N - 1; i >= 0; i--)
   {
-    c[i] = b[i] * fact[N - 1 - i] % MOD;
+    c[i] = (b[i] * fact[N - 1 - i]) % MOD;
     c[i] += c[i + 1];
     c[i] %= MOD;
   }
