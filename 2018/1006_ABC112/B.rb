874374c0fa2c5c256358513fc353e4eb4f6afe85
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-10-06 21:03:55 +0900

    submit B.rb to 'B - Time Limit Exceeded' (abc112) [Ruby (2.3.3)]

--- 2018/1006_ABC112/B.rb
+++ 2018/1006_ABC112/B.rb
@@ -5,11 +5,11 @@
 # Powered by Visual Studio Code
 #
 
-n, t = gets.chomp.split(" ").map{|i| i.to_i}
+n, l = gets.chomp.split(" ").map{|i| i.to_i}
 ans = 1000000
 n.times{
   c, t = gets.chomp.split(" ").map{|i| i.to_i}
-  if t <= T
+  if t <= l
     ans = [ans, c].min
   end
 }
