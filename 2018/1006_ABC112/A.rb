8a53f557159f54b4ed51c5b238a66ee1d74370a6
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-10-06 21:01:49 +0900

    submit A.rb to 'A - Programming Education' (abc112) [Ruby (2.3.3)]

---
+++ 2018/1006_ABC112/A.rb
@@ -0,0 +1,16 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 2018-10-6 20:58:24
+# Powered by Visual Studio Code
+#
+
+n = gets.to_i
+
+if n == 1
+  puts "Hello World"
+else
+  a = gets.to_i
+  b = gets.to_i
+  puts a+b
+end
