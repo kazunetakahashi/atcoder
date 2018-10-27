7c7c263bc7b545ee5af6254305f63c012643d035
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-10-27 21:29:55 +0900

    tried C.cpp to 'C'

--- 2018/1027_tenka1-2018/C.cpp
+++ 2018/1027_tenka1-2018/C.cpp
@@ -54,17 +54,17 @@ int main()
   {
     if (i % 2 == 0)
     {
-      B[i / 2] = A[i];
+      B[i] = A[i / 2];
     }
     else
     {
-      B[N - i / 2 - 1] = A[i];
+      B[i] = A[N - i / 2 - 1];
     }
   }
   ll sum = 0;
   for (auto i = 0; i < N - 1; i++)
   {
-    cerr << "B[" << i << "] = " << B[i] << endl;
+    // cerr << "B[" << i << "] = " << B[i] << endl;
     sum += abs(B[i] - B[i + 1]);
   }
   cout << sum << endl;
