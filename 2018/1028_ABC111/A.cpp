b7dbd1900a8dcf6d4e3721331a092cf7d5424f4c
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-10-28 21:57:20 +0900

    submit A.cpp to 'A - AtCoder Beginner Contest 999' (abc111) [C++14 (GCC 5.4.1)]

---
+++ 2018/1028_ABC111/A.cpp
@@ -0,0 +1,58 @@
+/**
+ * File    : A.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2018-10-28 21:56:02
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <functional>
+#include <random> // auto rd = bind(uniform_int_distribution<int>(0, 9), mt19937(19920725));
+#include <chrono> // std::chrono::system_clock::time_point start_time, end_time;
+// start = std::chrono::system_clock::now();
+// double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+string S;
+
+int main()
+{
+  cin >> S;
+  for (auto i = 0; i < (int)S.size(); i++)
+  {
+    if (S[i] == '1')
+    {
+      S[i] = '9';
+    }
+    else if (S[i] == '9')
+    {
+      S[i] = '1';
+    }
+  }
+  cout << S << endl;
+}
