54bf64618c9b176523f5da10e561c56203ac6ce1
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-11-04 16:32:52 +0900

    submit A.cpp to 'A - Colorful Transceivers' (abc097) [C++14 (GCC 5.4.1)]

--- 2018/1104_ABC097/A.cpp
+++ 2018/1104_ABC097/A.cpp
@@ -42,7 +42,7 @@ int main()
 {
   int a, b, c, d;
   cin >> a >> b >> c >> d;
-  if ((abs(a - b) <= d || abs(b - c) <= d) && abs(a - c) <= d)
+  if ((abs(a - b) <= d && abs(b - c) <= d) || abs(a - c) <= d)
   {
     cout << "Yes" << endl;
   }
