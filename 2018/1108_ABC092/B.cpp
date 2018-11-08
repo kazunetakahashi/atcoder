b41a6aaed15232feb67c8220002488db61057564
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-11-08 20:01:35 +0900

    submit B.cpp to 'B - Chocolate' (abc092) [C++14 (GCC 5.4.1)]

---
+++ 2018/1108_ABC092/B.cpp
@@ -0,0 +1,59 @@
+/**
+ * File    : B.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2018-11-8 19:59:26
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <functional>
+#include <random> // auto rd = bind(uniform_int_distribution<int>(0, 9), mt19937(19920725));
+#include <chrono> // std::chrono::system_clock::time_point start_time, end_time;
+// start = std::chrono::system_clock::now();
+// double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int N;
+int D, X;
+int A[110];
+
+int main()
+{
+  cin >> N;
+  cin >> D >> X;
+  for (auto i = 0; i < N; i++)
+  {
+    cin >> A[i];
+  }
+  int ans = X;
+  for (auto i = 0; i < N; i++)
+  {
+    ans += (D + A[i] - 1) / A[i];
+  }
+  cout << ans << endl;
+}
