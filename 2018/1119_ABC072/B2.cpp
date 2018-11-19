a396e8c0519bba29a6aa7e5ae8c35f90ba79f41f
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-11-19 09:10:03 +0900

    submit B2.cpp to 'B - OddString' (abc072) [C++14 (GCC 5.4.1)]

---
+++ 2018/1119_ABC072/B2.cpp
@@ -0,0 +1,50 @@
+/**
+ * File    : B2.cpp
+ * Author  : Kazune Takahashi
+ * Created : 11/19/2018, 9:09:14 AM
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <functional>
+#include <random> // auto rd = bind(uniform_int_distribution<int>(0, 9), mt19937(19920725));
+#include <chrono> // std::chrono::system_clock::time_point start_time, end_time;
+// start = std::chrono::system_clock::now();
+// double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int main()
+{
+  string S;
+  cin >> S;
+  for (auto i = 0; i < (int)S.size(); i += 2)
+  {
+    cout << S[i];
+  }
+  cout << endl;
+}
