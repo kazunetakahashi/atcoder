bd5773b06b9178ccfd26a79eb20df6815e9b16e2
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-11-20 23:15:20 +0900

    submit B.rb to 'B - Comparison' (abc059) [Ruby (2.3.3)]

---
+++ 2018/1120_ABC059/B.rb
@@ -0,0 +1,17 @@
+#
+# File    : B.rb
+# Author  : Kazune Takahashi
+# Created : 11/20/2018, 11:14:05 PM
+# Powered by Visual Studio Code
+#
+
+a = gets.to_i
+b = gets.to_i
+
+if a > b
+  puts "GREATER"
+elsif a < b
+  puts "LESS"
+else
+  puts "EQUAL"
+end
