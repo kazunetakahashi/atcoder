39fbb5b72dc419ca1c88122bf96ef3602d566a53
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-11-20 23:17:34 +0900

    tried A.rb to 'A'

---
+++ 2018/1120_ABC059/A.rb
@@ -0,0 +1,17 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 11/20/2018, 11:16:27 PM
+# Powered by Visual Studio Code
+#
+
+s = gets.chomp.split(" ").map{|i| i.to_s}
+
+ans = ""
+
+s.each{|str|
+  ans += str[0].toupper
+}
+
+puts ans
+
