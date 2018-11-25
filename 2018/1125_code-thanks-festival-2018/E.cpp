cf88ff4429292039f5b1bab6d2e44cc2e03c3453
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-11-25 12:28:37 +0900

    tried E.cpp to 'E'

---
+++ 2018/1125_code-thanks-festival-2018/E.cpp
@@ -0,0 +1,88 @@
+/**
+ * File    : E.cpp
+ * Author  : Kazune Takahashi
+ * Created : 11/25/2018, 12:14:31 PM
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <functional>
+#include <random> // auto rd = bind(uniform_int_distribution<int>(0, 9), mt19937(19920725));
+#include <chrono> // std::chrono::system_clock::time_point start_time, end_time;
+// start = std::chrono::system_clock::now();
+// double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+const ll M = 1000000007;
+
+int T;
+ll a[310];
+ll DP[310][400];
+
+ll cnt(ll i, ll l)
+{
+  if (0 <= 2 * l && 2 * l <= a[i])
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int main()
+{
+  cin >> T;
+  for (auto i = 1; i <= T; i++)
+  {
+    cin >> a[i];
+  }
+  for (auto i = 0; i < 400; i++)
+  {
+    DP[0][i] = 0;
+  }
+  DP[0][0] = 1;
+  for (auto i = 1; i <= T; i++)
+  {
+    for (auto j = 0; j < 400; j++)
+    {
+      DP[i][j] = 0;
+      for (auto k = 0; k <= j; k++)
+      {
+        DP[i][j] += DP[i - 1][k] * cnt(i, j - k);
+        DP[i][j] %= M;
+      }
+    }
+  }
+  ll ans = 0;
+  for (auto i = 1; i <= T; i++)
+  {
+    ans += DP[i][1];
+    ans %= M;
+  }
+  cout << ans << endl;
+}
