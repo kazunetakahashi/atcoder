73975d213c9a39c98c566e8182f9b35fe11b9ccd
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-12-02 21:34:48 +0900

    tried B2.rb to 'B'

---
+++ 2018/1202_ABC114/B2.rb
@@ -0,0 +1,14 @@
+#
+# File    : B2.rb
+# Author  : Kazune Takahashi
+# Created : 2018-12-2 21:33:52
+# Powered by Visual Studio Code
+#
+
+s = gets.chomp
+ans = []
+for i in 0...s.size - 3
+  ans << (753 - s[i...i + 3].to_i).abs
+end
+puts ans.min
+
