bb7da7951f1f979b134976b495e20836418f9a38
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-12-02 21:01:02 +0900

    tried A.rb to 'A'

---
+++ 2018/1202_ABC114/A.rb
@@ -0,0 +1,14 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 2018-12-2 20:31:55
+# Powered by Visual Studio Code
+#
+
+x = gets.to_i
+
+if x == 3 || x == 5 || x == 7
+  puts "Yes"
+else
+  puts "No"
+end
