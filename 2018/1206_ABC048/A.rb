936f365706eba4497b8e2252aed31e9199c015b3
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-12-06 23:06:08 +0900

    submit A.rb to 'A - AtCoder *** Contest' (abc048) [Ruby (2.3.3)]

---
+++ 2018/1206_ABC048/A.rb
@@ -0,0 +1,9 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 12/6/2018, 11:05:22 PM
+# Powered by Visual Studio Code
+#
+
+s = gets.chomp.split(" ").map{|i| i.chomp}
+puts s[0][0] + s[1][0] + s[2][0]
