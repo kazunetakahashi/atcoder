6caaf53b9d3a8087e513e7a4644db018e9b66dd3
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-12-08 21:11:51 +0900

    submit A.rb to 'A - Christmas Eve Eve Eve' (abc115) [Ruby (2.3.3)]

---
+++ 2018/1208_ABC115/A.rb
@@ -0,0 +1,10 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 12/8/2018, 8:24:58 PM
+# Powered by Visual Studio Code
+#
+
+str = ['Christmas Eve Eve Eve', "Christmas Eve Eve", "Christmas Eve", "Christmas"]
+
+puts str[gets.to_i - 22]
