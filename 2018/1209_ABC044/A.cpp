d600cfc010174516023161fcdc081e9db53ce735
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-12-09 22:29:21 +0900

    submit A.cpp to 'A - 高橋君とホテルイージー / Tak and Hotels (ABC Edit)' (abc044) [C++14 (GCC 5.4.1)]

---
+++ 2018/1209_ABC044/A.cpp
@@ -0,0 +1,59 @@
+/**
+ * File    : A.cpp
+ * Author  : Kazune Takahashi
+ * Created : 12/9/2018, 10:28:17 PM
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <functional>
+#include <random> // auto rd = bind(uniform_int_distribution<int>(0, 9), mt19937(19920725));
+#include <chrono> // std::chrono::system_clock::time_point start_time, end_time;
+// start = std::chrono::system_clock::now();
+// double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+ll N, K, X, Y;
+
+int main()
+{
+  cin >> N >> K >> X >> Y;
+  ll ans = 0;
+  for (auto i = 0; i < N; i++)
+  {
+    if (i < K)
+    {
+      ans += X;
+    }
+    else
+    {
+      ans += Y;
+    }
+  }
+  cout << ans << endl;
+}
