b1f3299260698b5d68fc04ce4732246a9df9a109
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-12-10 22:43:54 +0900

    tried B.rb to 'B'

---
+++ 2018/1210_ABC009/B.rb
@@ -0,0 +1,16 @@
+#
+# File    : B.rb
+# Author  : Kazune Takahashi
+# Created : 12/10/2018, 10:41:05 PM
+# Powered by Visual Studio Code
+#
+
+n = gets.to_i
+hash = {}
+n.times{
+  a = gets.to_i
+  if !hash.has_key?(a)
+    hash[a] = 1
+  end
+}
+puts hash.keys.sort[1]
