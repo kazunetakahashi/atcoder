2fc822b3200d6c31709ba63a510b2997c9df9086
Kazune Takahashi <kazunetakahashi@gmail.com>
2018-12-29 21:53:17 +0900

    tried B.cpp to 'B'

--- 2018/1229_AGC030/B.cpp
+++ 2018/1229_AGC030/B.cpp
@@ -52,29 +52,29 @@ void solve()
   for (auto i = 1; i <= N; i++)
   {
     sum[i] = sum[i - 1] + X[i];
-    cerr << "sum[" << i << "] = " << sum[i] << endl;
+    // cerr << "sum[" << i << "] = " << sum[i] << endl;
   }
   sum_rev[N + 1] = 0;
   for (auto i = N; i >= 1; i--)
   {
     sum_rev[i] = sum_rev[i + 1] + L - X[i];
-    cerr << "sum_rev[" << i << "] = " << sum_rev[i] << endl;
+    // cerr << "sum_rev[" << i << "] = " << sum_rev[i] << endl;
   }
-  for (auto right = 0; right <= N; right++)
+  for (auto right = 1; right <= N; right++)
   {
     int left = N - right;
-    int num = min(right + 1, left);
-    cerr << "left = " << left << ", right = " << right << endl;
-    ll tans = sum[right] - sum[right - (num - 1)] + sum_rev[right + 1] - sum_rev[right + 1 + num];
+    int num = min(right, left);
+    // cerr << "left = " << left << ", right = " << right << endl;
+    ll tans = sum[right] - sum[right - num] + sum_rev[right + 1] - sum_rev[right + 1 + num];
     tans *= 2;
     tans -= L - X[right + 1 + num];
-    cerr << "tans = " << tans << endl;
+    // cerr << "tans = " << tans << endl;
     ans = max(tans, ans);
     num = min(right, left + 1);
     tans = sum[right] - sum[right - num] + sum_rev[right + 1] - sum_rev[right + 1 + (num - 1)];
     tans *= 2;
     tans -= X[right - num];
-    cerr << "tans = " << tans << endl;
+    // cerr << "tans = " << tans << endl;
     ans = max(tans, ans);
   }
 }
