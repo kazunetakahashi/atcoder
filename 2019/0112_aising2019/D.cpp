eb41abe8c77df0143d6baef2ee534dd0d4451fcb
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-01-12 22:23:00 +0900

    tried D.cpp to 'D'

---
+++ 2019/0112_aising2019/D.cpp
@@ -0,0 +1,126 @@
+/**
+ * File    : D.cpp
+ * Author  : Kazune Takahashi
+ * Created : 1/12/2019, 9:15:17 PM
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <functional>
+#include <random> // auto rd = bind(uniform_int_distribution<int>(0, 9), mt19937(19920725));
+#include <chrono> // std::chrono::system_clock::time_point start_time, end_time;
+// start = std::chrono::system_clock::now();
+// double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+#define DEBUG 0 // change 0 -> 1 if we need debug.
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+const ll infty = 1000000007;
+
+int N, Q;
+ll A[100010];
+ll X[100010];
+set<ll> S;
+map<ll, ll> M;
+ll Y[100010];
+ll Z[100010];
+
+void flush()
+{
+  for (auto i = 0; i < Q; i++)
+  {
+    cout << M[X[i]] << endl;
+  }
+}
+
+int main()
+{
+  cin >> N >> Q;
+  for (auto i = 0; i < N; i++)
+  {
+    cin >> A[i];
+  }
+  for (auto i = 0; i < Q; i++)
+  {
+    cin >> X[i];
+    S.insert(X[i]);
+  }
+  reverse(X, X + N);
+  Y[N + 1] = 0;
+  Y[N] = 0;
+  Y[N - 1] = X[N - 1];
+  Y[N - 2] = Y[N - 2];
+  for (auto i = N - 3; i >= 0; i--)
+  {
+    Y[i] = X[i] + Y[i + 2];
+  }
+  Z[0] = X[0];
+  for (auto i = 1; i < N; i++)
+  {
+    Z[i] = X[i] + Z[i - 1];
+  }
+  auto it = S.begin();
+  for (auto i = N; i >= 0; i--)
+  {
+    int second = i / 2;
+    int first = i - second;
+    ll score;
+    if (first == 0)
+    {
+      score = Y[0];
+    }
+    else if (first == second)
+    {
+      score = Z[first - 1] + Y[i];
+    }
+    else
+    {
+      score = Z[first - 1] + Y[i + 1];
+    }
+    ll upper;
+    if (second == 0)
+    {
+      upper = infty;
+    }
+    else
+    {
+      ll maxi = X[first - 1];
+      ll mini = X[first + second - 1];
+      upper = (maxi + mini + 1) / 2;
+    }
+    while (it != S.end())
+    {
+      if (*it <= upper)
+      {
+        M[*it] = score;
+        it++;
+      }
+      else
+      {
+        break;
+      }
+    }
+  }
+  flush();
+}
