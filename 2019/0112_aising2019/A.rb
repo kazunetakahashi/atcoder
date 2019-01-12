778fa1bf45943c8b678c4f5a524acd649ba42aa6
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-01-12 21:02:05 +0900

    submit A.rb to 'A - Bulletin Board' (aising2019) [Ruby (2.3.3)]

---
+++ 2019/0112_aising2019/A.rb
@@ -0,0 +1,12 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 1/12/2019, 8:52:56 PM
+# Powered by Visual Studio Code
+#
+
+n = gets.to_i
+h = gets.to_i
+w = gets.to_i
+
+puts (n - h + 1) * (n - w + 1)
