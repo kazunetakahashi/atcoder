104e9e83fe561a66258e934e4e806e0547aa9fe2
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-01-20 21:34:11 +0900

    submit A2.rb to 'A - Right Triangle' (abc116) [Ruby (2.3.3)]

---
+++ 2019/0120_ABC116/A2.rb
@@ -0,0 +1,9 @@
+#
+# File    : A2.rb
+# Author  : Kazune Takahashi
+# Created : 1/20/2019, 9:33:40 PM
+# Powered by Visual Studio Code
+#
+
+x = gets.chomp.split(" ").map{|i| i.to_i}
+puts x[0] * x[1] / 2
