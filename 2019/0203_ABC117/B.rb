7ce96849122cc6a91e87075725d44978f01e75f4
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-02-03 21:04:09 +0900

    tried B.rb to 'B'

---
+++ 2019/0203_ABC117/B.rb
@@ -0,0 +1,16 @@
+#
+# File    : B.rb
+# Author  : Kazune Takahashi
+# Created : 2/3/2019, 9:02:53 PM
+# Powered by Visual Studio Code
+#
+
+n = gets.to_i
+l = gets.chomp.split(" ").map{|i| i.to_i}
+sum = l.inject(:+)
+maxi = l.maxi
+if sum - maxi > maxi
+  puts "Yes"
+else
+  puts "No"
+end
