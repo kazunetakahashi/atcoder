e0a11169cbfaa57734c9338687ae810a223399e5
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-02-16 21:01:06 +0900

    tried A.rb to 'A'

---
+++ 2019/0216_ABC118/A.rb
@@ -0,0 +1,13 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 2/16/2019, 8:51:35 PM
+# Powered by Visual Studio Code
+#
+
+a, b = gets.chomp.split(" ").map{|i| i.to_i}
+if a % b == 0
+  puts a + b
+else
+  puts b - a
+end
