695541497520e0160b1967b6354ad423c31a9ee3
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-02-16 21:05:31 +0900

    tried C.rb to 'C'

---
+++ 2019/0216_ABC118/C.rb
@@ -0,0 +1,10 @@
+#
+# File    : C.rb
+# Author  : Kazune Takahashi
+# Created : 2/16/2019, 9:04:43 PM
+# Powered by Visual Studio Code
+#
+
+n = gets.to_i
+a = gets.chomp.split(" ").map{|i| i.to_i}
+puts a.lcm
