fcba1ffff690df2f9858f63f5f4612093ac0c86c
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-02-17 15:35:11 +0900

    tried E2.cpp to 'E'

--- 2019/0217_nikkei2019-final/E2.cpp
+++ 2019/0217_nikkei2019-final/E2.cpp
@@ -111,15 +111,15 @@ int main()
       f = -1;
     }
     ll k = i * (i + 1) / 2;
+    k %= MOD;
+    ans += (MOD + f * (C(L, i) * power(2, k)) % MOD) % MOD;
 #if DEBUG == 1
     if (L < 10)
     {
       cerr << "i = " << i << ", k = " << k << ", power(2, " << k << ") = " << power(2, k) << ", C(" << L << ", " << i << ") = " << C(L, i) << endl;
+      cerr << "ans = " << ans << endl;
     }
 #endif
-    k %= MOD;
-    ans += (MOD + f * (C(L, i) * power(2, k)) % MOD) % MOD;
-    cerr << "ans = " << ans << endl;
   }
   cout << ans << endl;
 }
