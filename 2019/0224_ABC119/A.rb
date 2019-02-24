c304c920582384090727085aacb2a842a4fa21dd
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-02-24 21:01:43 +0900

    submit A.rb to 'A - Still TBD' (abc119) [Ruby (2.3.3)]

---
+++ 2019/0224_ABC119/A.rb
@@ -0,0 +1,13 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 2/24/2019, 8:54:51 PM
+# Powered by Visual Studio Code
+#
+
+s = gets.chomp
+if s <= "2019/04/30"
+  puts "Heisei"
+else
+  puts "TBD"
+end
