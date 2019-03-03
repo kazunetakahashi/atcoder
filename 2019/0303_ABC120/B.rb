eb27980518340948caa65860742ee52cdc80d5bc
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-03-03 21:03:30 +0900

    tried B.rb to 'B'

---
+++ 2019/0303_ABC120/B.rb
@@ -0,0 +1,18 @@
+#
+# File    : B.rb
+# Author  : Kazune Takahashi
+# Created : 3/3/2019, 9:02:17 PM
+# Powered by Visual Studio Code
+#
+
+a, b, k = gets.chomp.split(" ").map{|i| i.to_i}
+
+for i in 1..[a, b].max
+  if a % i == 0 && b % i == 0
+    k -= 1
+  end
+  if k == 0
+    puts i
+    exit
+  end
+end
