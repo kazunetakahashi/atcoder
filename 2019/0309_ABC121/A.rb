eea5a00c0afa4b61470b2fed9173df6107ce229c
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-03-09 21:01:34 +0900

    tried A.rb to 'A'

---
+++ 2019/0309_ABC121/A.rb
@@ -0,0 +1,10 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 3/9/2019, 8:47:41 PM
+# Powered by Visual Studio Code
+#
+
+h, w = gets.chomp.split(" ").map{|i| i.to_i}
+x, y = gets.chomp.split(" ").map{|i| i.to_i}
+puts (h - x) * (w - t)
