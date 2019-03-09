f5b44cee4b89c4d14ae9bec5d647dac645326644
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-03-09 23:06:29 +0900

    tried D2.cpp to 'D'

---
+++ 2019/0309_ABC121/D2.cpp
@@ -0,0 +1,69 @@
+#define DEBUG 1
+
+/**
+ * File    : D2.cpp
+ * Author  : Kazune Takahashi
+ * Created : 3/9/2019, 11:03:51 PM
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <functional>
+#include <random> // auto rd = bind(uniform_int_distribution<int>(0, 9), mt19937(10101010));
+#include <chrono> // std::chrono::system_clock::time_point start_time, end_time;
+// start = std::chrono::system_clock::now();
+// double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+ll A, B;
+
+ll F(ll x)
+{
+  if (x < 0)
+  {
+    return 0;
+  }
+  ll ans;
+  if (x % 4 == 2 || x % 4 == 3)
+  {
+    ans = 1;
+  }
+  else
+  {
+    ans = 0;
+  }
+  if (x % 2 == 1)
+  {
+    ans ^= x;
+  }
+  return ans;
+}
+
+int main()
+{
+  cin >> A >> B;
+  cout << (F(A - 1) ^ F(B)) << endl;
+}
