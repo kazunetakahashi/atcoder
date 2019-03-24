9ab3e2f2ab6429f032352474227f46ce879c011b
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-03-24 21:01:56 +0900

    submit A.rb to 'A - Double Helix' (abc122) [Ruby (2.3.3)]

---
+++ 2019/0324_ABC122/A.rb
@@ -0,0 +1,17 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 2019-3-24 20:58:56
+# Powered by Visual Studio Code
+#
+
+b = gets.chomp
+if b == 'A'
+  puts 'T'
+elsif b == 'T'
+  puts 'A'
+elsif b == 'C'
+  puts 'G'
+else
+  puts 'C'
+end
