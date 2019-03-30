223e7088610e8dcd3307d4833833205faad72ea0
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-03-30 21:03:39 +0900

    submit B.cpp to 'B - Red or Blue' (exawizards2019) [C++14 (GCC 5.4.1)]

---
+++ 2019/0330_exawizards2019/B.cpp
@@ -0,0 +1,67 @@
+#define DEBUG 1
+
+/**
+ * File    : B.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2019-3-30 21:02:20
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <functional>
+#include <random> // auto rd = bind(uniform_int_distribution<int>(0, 9), mt19937(10101010));
+#include <chrono> // std::chrono::system_clock::time_point start_time, end_time;
+// start = std::chrono::system_clock::now();
+// double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int N;
+string S;
+
+int main()
+{
+  cin >> N >> S;
+  int r = 0, b = 0;
+  for (auto x : S)
+  {
+    if (x == 'R')
+    {
+      r++;
+    }
+    else
+    {
+      b++;
+    }
+  }
+  if (r > b)
+  {
+    cout << "Yes" << endl;
+  }
+  else
+  {
+    cout << "No" << endl;
+  }
+}
