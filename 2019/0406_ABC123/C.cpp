762c50bd895e27a23f99968fd9e4da5234b66814
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-04-06 21:14:12 +0900

    tried C.cpp to 'C'

---
+++ 2019/0406_ABC123/C.cpp
@@ -0,0 +1,60 @@
+#define DEBUG 1
+
+/**
+ * File    : C.cpp
+ * Author  : Kazune Takahashi
+ * Created : 4/6/2019, 9:08:37 PM
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <functional>
+#include <random> // auto rd = bind(uniform_int_distribution<int>(0, 9), mt19937(10101010));
+#include <chrono> // std::chrono::system_clock::time_point start_time, end_time;
+// start = std::chrono::system_clock::now();
+// double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+ll N;
+ll A[5];
+ll ind = 0;
+ll mini;
+
+int main()
+{
+  cin >> N;
+  for (auto i = 0; i < 5; i++)
+  {
+    cin >> A[i];
+  }
+  mini = A[0];
+  for (auto i = 0; i < 5; i++)
+  {
+    mini = min(mini, A[i]);
+  }
+  ll ans = (N + mini - 1) / mini;
+  cout << ans + 5 << endl;
+}
