6562c19b55ad117b4708c3c491cfa90b2d480d7e
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-04-13 21:03:43 +0900

    tried B.rb to 'B'

---
+++ 2019/0413_ABC124/B.rb
@@ -0,0 +1,27 @@
+#
+# File    : B.rb
+# Author  : Kazune Takahashi
+# Created : 4/13/2019, 9:01:56 PM
+# Powered by Visual Studio Code
+#
+
+n = gets.to_i
+h = gets.chomp.split(" ").map{|i| i.to_i}
+ans = 0
+for i in 0...n
+  if i == 0
+    ans += 1
+  else
+    ok = true
+    for j in 0...i
+      if h[j] > h[j + 1]
+        ok = false
+        break
+      end
+    end
+    if ok
+      ans += 1
+    end
+  end
+end
+puts ans
