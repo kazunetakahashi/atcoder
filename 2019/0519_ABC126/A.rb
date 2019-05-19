f0bf3eabb0a524cedcba6f236b5d2140b48e3ceb
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-05-19 21:03:05 +0900

    tried A.rb to 'A'

---
+++ 2019/0519_ABC126/A.rb
@@ -0,0 +1,11 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 5/19/2019, 8:46:54 PM
+# Powered by Visual Studio Code
+#
+
+n, k = gets.chomp.split(" ").map{|i| i.to_i}
+s = gets.chomp
+s[k - 1] = s[k - 1].lowercase
+puts s
