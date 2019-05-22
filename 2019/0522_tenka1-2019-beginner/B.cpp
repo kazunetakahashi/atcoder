4f1123c4db75a6025326327a625703171e47693c
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-05-22 16:33:26 +0900

    tried B.cpp to 'B'

---
+++ 2019/0522_tenka1-2019-beginner/B.cpp
@@ -0,0 +1,57 @@
+#define DEBUG 1
+
+/**
+ * File    : B.cpp
+ * Author  : Kazune Takahashi
+ * Created : 5/22/2019, 4:32:20 PM
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <functional>
+#include <random> // auto rd = bind(uniform_int_distribution<int>(0, 9), mt19937(10101010));
+#include <chrono> // std::chrono::system_clock::time_point start_time, end_time;
+// start = std::chrono::system_clock::now();
+// double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+int N, K;
+string S;
+
+int main()
+{
+  cin >> N >> S >> K;
+  K--;
+  char x = S[K];
+  for (auto i = 0; i < N; i++)
+  {
+    if (S[i] == x)
+    {
+      S[i] = '*';
+    }
+  }
+  cout << S << endl;
+}
