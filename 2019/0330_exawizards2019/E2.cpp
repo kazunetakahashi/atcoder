a61259437d1cff1d39151af44938064227f8dcb4
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-05-25 00:53:11 +0900

    tried E2.cpp to 'E'

---
+++ 2019/0330_exawizards2019/E2.cpp
@@ -0,0 +1,115 @@
+#define DEBUG 1
+
+/**
+ * File    : E2.cpp
+ * Author  : Kazune Takahashi
+ * Created : 5/25/2019, 12:46:38 AM
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>   // << fixed << setprecision(xxx)
+#include <algorithm> // do { } while ( next_permutation(A, A+xxx) ) ;
+#include <vector>
+#include <string> // to_string(nnn) // substr(m, n) // stoi(nnn)
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map> // if (M.find(key) != M.end()) { }
+#include <set>
+#include <functional>
+#include <random> // auto rd = bind(uniform_int_distribution<int>(0, 9), mt19937(10101010));
+#include <chrono> // std::chrono::system_clock::time_point start_time, end_time;
+// start = std::chrono::system_clock::now();
+// double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+typedef long long ll;
+
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+// const int C = 1e6+10;
+// const ll M = 1000000007;
+
+const int MAX_SIZE = 1000010;
+const long long MOD = 1000000007;
+
+long long inv[MAX_SIZE];
+long long fact[MAX_SIZE];
+long long factinv[MAX_SIZE];
+
+void init()
+{
+  inv[1] = 1;
+  for (int i = 2; i < MAX_SIZE; i++)
+  {
+    inv[i] = ((MOD - inv[MOD % i]) * (MOD / i)) % MOD;
+  }
+  fact[0] = factinv[0] = 1;
+  for (int i = 1; i < MAX_SIZE; i++)
+  {
+    fact[i] = (i * fact[i - 1]) % MOD;
+    factinv[i] = (inv[i] * factinv[i - 1]) % MOD;
+  }
+}
+
+long long C(int n, int k)
+{
+  if (n >= 0 && k >= 0 && n - k >= 0)
+  {
+    return ((fact[n] * factinv[k]) % MOD * factinv[n - k]) % MOD;
+  }
+  return 0;
+}
+
+long long power(long long x, long long n)
+{
+  if (n == 0)
+  {
+    return 1;
+  }
+  else if (n % 2 == 1)
+  {
+    return (x * power(x, n - 1)) % MOD;
+  }
+  else
+  {
+    long long half = power(x, n / 2);
+    return (half * half) % MOD;
+  }
+}
+
+long long gcd(long long x, long long y)
+{
+  return y ? gcd(y, x % y) : x;
+}
+
+ll B, W;
+ll ans[200010];
+
+int main()
+{
+  init();
+  cin >> B >> W;
+  ll N = B + W;
+  ans[0] = inv[2];
+  for (auto k = 0; k < N - 1; k++)
+  {
+    ans[k + 1] = ans[k];
+    ans[k + 1] += MOD - (C(k - 1, B - 1) * power(inv[2], k + 1)) % MOD;
+    ans[k + 1] %= MOD;
+    ans[k + 1] += (C(k - 1, W - 1) * power(inv[2], k + 1)) % MOD;
+    ans[k + 1] %= MOD;
+  }
+  for (auto i = 1; i <= N; i++)
+  {
+    cout << ans[i] << endl;
+  }
+}
