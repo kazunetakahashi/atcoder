52cdf71479c76b7ad36ab93572912ea2aafa7f1e
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-05-25 22:31:16 +0900

    tried F.cpp to 'F'

--- 2019/0525_ABC127/F.cpp
+++ 2019/0525_ABC127/F.cpp
@@ -148,8 +148,8 @@ void flush()
 {
   ll val = L.top();
   ll ans = B;
-  ans -= bit.sum(M[val]);
-  ans += bit.sum(M[val], W.size());
+  ans -= bit.sum(M[val] - 1);
+  ans += bit.sum(M[val] - 1, W.size());
 #if DEBUG == 1
   cerr << "M[" << val << "] = " << M[val] << ", W.size() = " << W.size() << endl;
   cerr << "bit.sum(" << M[val] << ") = " << bit.sum(M[val]) << endl;
