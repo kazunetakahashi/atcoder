0ea3e071d022ce310012d0c8de066c11b889df2c
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-05-26 21:04:38 +0900

    tried B.rb to 'B'

---
+++ 2019/0526_ABC128/B.rb
@@ -0,0 +1,17 @@
+#
+# File    : B.rb
+# Author  : Kazune Takahashi
+# Created : 2019-5-26 21:03:02
+# Powered by Visual Studio Code
+#
+
+n = gets.chomp.split(" ").map{|i| i.to_i}
+ary = []
+for i in 1..n
+  s, q = gets.chomp.split(" ").map{|i| i.to_s}
+  q = q.to_i
+  ary << [s, -q, i]
+end
+ary.sort.each{|s, q, i|
+  puts i
+}
