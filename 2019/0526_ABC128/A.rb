ddcbca13b9fabf07a740df0a167a648cbeb28895
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-05-26 21:02:42 +0900

    submit A.rb to 'A - Apple Pie' (abc128) [Ruby (2.3.3)]

---
+++ 2019/0526_ABC128/A.rb
@@ -0,0 +1,9 @@
+#
+# File    : A.rb
+# Author  : Kazune Takahashi
+# Created : 2019-5-26 20:53:52
+# Powered by Visual Studio Code
+#
+
+a, p = gets.chomp.split(" ").map{|i| i.to_i}
+puts (3 * a + p) / 2
