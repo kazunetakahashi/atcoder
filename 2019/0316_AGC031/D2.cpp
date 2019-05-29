f497b501855e54ca2db65d219929e94b5e28f5c7
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-05-29 20:50:21 +0900

    tried D2.cpp to 'D'

---
+++ 2019/0316_AGC031/D2.cpp
@@ -0,0 +1,153 @@
+#define DEBUG 1
+
+/**
+ * File    : D2.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2019-5-29 20:27:35
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <random>
+#include <chrono>
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+typedef long long ll;
+
+/*
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+
+void No()
+{
+  cout << "No" << endl;
+  exit(0);
+}
+*/
+
+/*
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+*/
+
+// const ll MOD = 1000000007;
+
+int N, K;
+vector<int> a, b, A, B, x, y, I;
+
+vector<int> inv(vector<int> V)
+{
+  vector<int> W(N);
+  for (auto i = 0; i < N; i++)
+  {
+    W[V[i]] = i;
+  }
+  return W;
+}
+
+vector<int> mult(vector<int> V, vector<int> W)
+{
+  vector<int> U(N);
+  for (auto i = 0; i < N; i++)
+  {
+    U[i] = V[W[i]];
+  }
+  return U;
+}
+
+vector<int> power(vector<int> V, int n)
+{
+  if (n == 0)
+  {
+    return I;
+  }
+  else if (n % 2 == 0)
+  {
+    vector<int> W = power(V, n / 2);
+    return mult(W, W);
+  }
+  else
+  {
+    return mult(V, power(V, n - 1));
+  }
+}
+
+vector<int> f(vector<int> V, vector<int> W)
+{
+  return mult(W, inv(V));
+}
+
+void flush(vector<int> V)
+{
+  for (auto i = 0; i < N; i++)
+  {
+    cout << V[i];
+    if (i < N - 1)
+    {
+      cout << " ";
+    }
+    else
+    {
+      cout << endl;
+    }
+  }
+}
+
+int main()
+{
+  cin >> N >> K;
+  K--;
+  for (auto i = 0; i < N; i++)
+  {
+    I[i] = i;
+  }
+  for (auto i = 0; i < N; i++)
+  {
+    cin >> a[i];
+    a[i]--;
+  }
+  for (auto i = 0; i < N; i++)
+  {
+    cin >> b[i];
+    b[i]--;
+  }
+  A = inv(a);
+  B = inv(b);
+  // $x = bABa$, $y = AbaB$
+  x = mult(mult(b, A), mult(B, a));
+  y = mult(mult(A, b), mult(a, B));
+  if (K == 0)
+  {
+    flush(a);
+    return 0;
+  }
+  int n = (K - 1) / 6;
+  vector<int> p = mult(mult(power(x, n), b), power(y, n));
+  vector<int> q = mult(mult(power(x, n), mult(b, A)), power(y, n));
+  for (auto i = 6 * n + 1; i < K; i++)
+  {
+    vector<int> r = f(p, q);
+    p = q;
+    q = r;
+  }
+  flush(p);
+}
