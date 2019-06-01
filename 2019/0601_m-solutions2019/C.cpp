f9ee3fd45f61cb129dc24811f24b708b042074a7
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-06-01 22:53:51 +0900

    tried C.cpp to 'C'

---
+++ 2019/0601_m-solutions2019/C.cpp
@@ -0,0 +1,123 @@
+#define DEBUG 1
+
+/**
+ * File    : C.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2019-6-1 22:49:57
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <random>
+#include <chrono>
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+typedef long long ll;
+
+/*
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+
+void No()
+{
+  cout << "No" << endl;
+  exit(0);
+}
+*/
+
+/*
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+*/
+
+// const ll MOD = 1000000007;
+
+const int MAX_SIZE = 1000010;
+const long long MOD = 1000000007;
+
+long long inv[MAX_SIZE];
+long long fact[MAX_SIZE];
+long long factinv[MAX_SIZE];
+
+void init()
+{
+  inv[1] = 1;
+  for (int i = 2; i < MAX_SIZE; i++)
+  {
+    inv[i] = ((MOD - inv[MOD % i]) * (MOD / i)) % MOD;
+  }
+  fact[0] = factinv[0] = 1;
+  for (int i = 1; i < MAX_SIZE; i++)
+  {
+    fact[i] = (i * fact[i - 1]) % MOD;
+    factinv[i] = (inv[i] * factinv[i - 1]) % MOD;
+  }
+}
+
+long long C(int n, int k)
+{
+  if (n >= 0 && k >= 0 && n - k >= 0)
+  {
+    return ((fact[n] * factinv[k]) % MOD * factinv[n - k]) % MOD;
+  }
+  return 0;
+}
+
+long long power(long long x, long long n)
+{
+  if (n == 0)
+  {
+    return 1;
+  }
+  else if (n % 2 == 1)
+  {
+    return (x * power(x, n - 1)) % MOD;
+  }
+  else
+  {
+    long long half = power(x, n / 2);
+    return (half * half) % MOD;
+  }
+}
+
+long long gcd(long long x, long long y)
+{
+  return y ? gcd(y, x % y) : x;
+}
+
+ll N, A, B, K;
+ll ans = 0;
+
+int main()
+{
+  cin >> N >> A >> B >> K;
+  ll X = (A * inv[A + B]) % MOD;
+  ll Y = (B * inv[A + B]) % MOD;
+  for (auto n = 0; n <= N; n++)
+  {
+    ans += (((power(X, n) * power(Y, N - n)) % MOD) * C(N, n)) % MOD;
+    ans %= MOD;
+  }
+  ans *= (100 * inv[K]);
+  ans %= MOD;
+  cout << ans << endl;
+}
