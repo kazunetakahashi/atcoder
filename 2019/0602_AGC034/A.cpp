2d3f74543bdb62924a0cbdb8de0e02d834c6fc19
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-06-02 21:08:45 +0900

    tried A.cpp to 'A'

---
+++ 2019/0602_AGC034/A.cpp
@@ -0,0 +1,100 @@
+#define DEBUG 1
+
+/**
+ * File    : A.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2019-6-2 20:42:31
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <random>
+#include <chrono>
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+typedef long long ll;
+
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+
+void No()
+{
+  cout << "No" << endl;
+  exit(0);
+}
+
+/*
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+*/
+
+// const ll MOD = 1000000007;
+
+string S;
+int A, B, C, D, N;
+
+bool solve(int X, int Y)
+{
+  for (auto i = X; i < Y; i++)
+  {
+    if (S[i] == '#' && S[i + 1] == '#')
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool solve2(int X, int Y, int Z, int W)
+{
+  for (auto i = Z; i < W - 1; i++)
+  {
+    if ((i == 0 || S[i - 1] == '.') && S[i] == '.' && S[i + 1] == '.')
+    {
+      S[i] = '#';
+      break;
+    }
+  }
+  return solve(X, Y);
+}
+
+int main()
+{
+  cin >> N >> A >> B >> C >> D >> S;
+  A--;
+  B--;
+  C--;
+  D--;
+  if (C < B)
+  {
+    if (solve(A, B) && solve(C, D))
+    {
+      Yes();
+    }
+    No();
+  }
+  if (solve(C, D) && solve2(A, B, C, D))
+  {
+    Yes();
+  }
+  No();
+}
