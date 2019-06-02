e7bee598dcf3c1149a1341965f894e235d23cd49
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-06-02 22:34:23 +0900

    tried C.cpp to 'C'

---
+++ 2019/0602_AGC034/C.cpp
@@ -0,0 +1,111 @@
+#define DEBUG 1
+
+/**
+ * File    : C.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2019-6-2 21:52:44
+ * Powered by Visual Studio Code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <random>
+#include <chrono>
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+
+typedef long long ll;
+
+/*
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+
+void No()
+{
+  cout << "No" << endl;
+  exit(0);
+}
+*/
+
+/*
+const int dx[4] = {1, 0, -1, 0};
+const int dy[4] = {0, 1, 0, -1};
+*/
+
+// const ll MOD = 1000000007;
+
+int N;
+ll X;
+ll b[100010];
+ll l[100010];
+ll u[100010];
+ll maxi = 0;
+ll now = 0;
+ll ans = 0;
+
+typedef tuple<ll, ll, ll, ll> card;  // maxi, b, l, u;
+typedef tuple<ll, ll, ll, ll> card2; // u, l, b, cost;
+vector<card> V;
+vector<card2> W;
+
+int main()
+{
+  cin >> N >> X;
+  for (auto i = 0; i < N; i++)
+  {
+    cin >> b[i] >> l[i] >> u[i];
+  }
+  for (auto i = 0; i < N; i++)
+  {
+    maxi += b[i] * l[i];
+  }
+  for (auto i = 0; i < N; i++)
+  {
+    ll t = (l[i] * b[i] + u[i] * (X - b[i]));
+    V.emplace_back(t, b[i], l[i], u[i]);
+  }
+  sort(V.begin(), V.end());
+  reverse(V.begin(), V.end());
+  int ind = 0;
+  while (maxi >= now)
+  {
+    ll t, b, l, u;
+    tie(t, b, l, u) = V[ind++];
+    now += t;
+    ans += X;
+    W.emplace_back(u, l, b, X);
+  }
+  sort(W.begin(), W.end());
+  ind = 0;
+  while (now >= maxi)
+  {
+    if (get<3>(W[0]) > get<2>(W[0]))
+    {
+      now -= get<0>(W[0]);
+    }
+    else
+    {
+      now -= get<1>(W[0]);
+    }
+    get<3>(W[0])--;
+    ans--;
+  }
+  cout << ans + 1 << endl;
+}
