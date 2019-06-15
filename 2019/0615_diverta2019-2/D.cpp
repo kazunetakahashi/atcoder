08e6c0447540d539ed4f661d90a316082faf0b94
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-06-15 22:16:24 +0900

    submit D.cpp to 'D - Squirrel Merchant' (diverta2019-2) [C++14 (GCC 5.4.1)]

---
+++ 2019/0615_diverta2019-2/D.cpp
@@ -0,0 +1,191 @@
+#define DEBUG 1
+/**
+ * File    : D.cpp
+ * Author  : Kazune Takahashi
+ * Created : 6/15/2019, 9:46:28 PM
+ * Powered by Visual Studio Code
+ */
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <random>
+#include <chrono>
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+typedef long long ll;
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+void No()
+{
+  cout << "No" << endl;
+  exit(0);
+}
+const int MAX_SIZE = 1000010;
+class mint
+{
+public:
+  static ll MOD;
+  ll x;
+  mint() : x(0) {}
+  mint(ll x) : x(x % MOD) {}
+  mint operator-() const { return mint(0) - *this; }
+  mint &operator+=(const mint &a)
+  {
+    if ((x += a.x) >= MOD)
+    {
+      x -= MOD;
+    }
+    return *this;
+  }
+  mint &operator-=(const mint &a) { return *this += -a; }
+  mint &operator*=(const mint &a)
+  {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint &operator/=(const mint &a) { return (*this *= power(MOD - 2)); }
+  mint operator+(const mint &a) const { return mint(*this) += a; }
+  mint operator-(const mint &a) const { return mint(*this) -= a; }
+  mint operator*(const mint &a) const { return mint(*this) *= a; }
+  mint operator/(const mint &a) const { return mint(*this) /= a; }
+  bool operator<(const mint &a) const { return x < a.x; }
+  bool operator==(const mint &a) const { return x == a.x; }
+  const mint power(ll N)
+  {
+    if (N == 0)
+    {
+      return 1;
+    }
+    else if (N % 2 == 1)
+    {
+      return *this * power(N - 1);
+    }
+    else
+    {
+      mint half = power(N / 2);
+      return half * half;
+    }
+  }
+};
+ll mint::MOD = 1e9 + 7;
+istream &operator>>(istream &stream, mint &a) { return stream >> a.x; }
+ostream &operator<<(ostream &stream, const mint &a) { return stream << a.x; }
+mint inv[MAX_SIZE];
+mint fact[MAX_SIZE];
+mint factinv[MAX_SIZE];
+void init()
+{
+  inv[1] = 1;
+  for (int i = 2; i < MAX_SIZE; i++)
+  {
+    inv[i] = (-inv[mint::MOD % i]) * (mint::MOD / i);
+  }
+  fact[0] = factinv[0] = 1;
+  for (int i = 1; i < MAX_SIZE; i++)
+  {
+    fact[i] = mint(i) * fact[i - 1];
+    factinv[i] = inv[i] * factinv[i - 1];
+  }
+}
+mint choose(int n, int k)
+{
+  if (n >= 0 && k >= 0 && n - k >= 0)
+  {
+    return fact[n] * factinv[k] * factinv[n - k];
+  }
+  return 0;
+}
+ll gcd(ll x, ll y) { return y ? gcd(y, x % y) : x; }
+// const double epsilon = 1e-10;
+// const ll infty = 1000000000000000LL;
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+ll N;
+ll X[2][3];
+ll ans;
+
+typedef tuple<ll, ll, ll, ll> state;
+
+state change(state x, int k, int t)
+{
+  if (k == 6)
+  {
+    return x;
+  }
+  ll n, g, s, b;
+  tie(n, g, s, b) = x;
+  if (k == 0)
+  {
+    g += (n / X[t][k / 2]);
+    n %= X[t][k / 2];
+  }
+  else if (k == 1)
+  {
+    n += X[t][k / 2] * g;
+    g = 0;
+  }
+  else if (k == 2)
+  {
+    s += (n / X[t][k / 2]);
+    n %= X[t][k / 2];
+  }
+  else if (k == 3)
+  {
+    n += X[t][k / 2] * s;
+    s = 0;
+  }
+  else if (k == 4)
+  {
+    b += (n / X[t][k / 2]);
+    n %= X[t][k / 2];
+  }
+  else if (k == 5)
+  {
+    n += X[t][k / 2] * b;
+    b = 0;
+  }
+  return tie(n, g, s, b);
+}
+
+int main()
+{
+  cin >> N;
+  ans = N;
+  cin >> X[0][0] >> X[0][1] >> X[0][2];
+  cin >> X[1][0] >> X[1][1] >> X[1][2];
+  for (auto i = 0; i < 7; i++)
+  {
+    for (auto j = 0; j < 7; j++)
+    {
+      for (auto k = 0; k < 7; k++)
+      {
+        state x = state(N, 0, 0, 0);
+        x = change(x, i, 0);
+        x = change(x, j, 1);
+        x = change(x, k, 0);
+        maxs(ans, get<0>(x));
+      }
+    }
+  }
+  cout << ans << endl;
+}
