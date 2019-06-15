e239adb48f31e7b7a9883f95a6d5117159c395b4
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-06-16 01:12:15 +0900

    tried E.cpp to 'E'

--- 2019/0615_diverta2019-2/E.cpp
+++ 2019/0615_diverta2019-2/E.cpp
@@ -99,6 +99,7 @@ void init()
   {
 #if DEBUG == 1
     cerr << mint::MOD % i << endl;
+    cerr << inv[mint::MOD % i] << endl;
     cerr << -inv[mint::MOD % i] << endl;
 #endif
     inv[i] = (-inv[mint::MOD % i]) * (mint::MOD / i);
