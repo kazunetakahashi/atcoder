143f09e8bd5db14c0f909615e26b091f7254dfe7
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-06-23 06:52:53 +0900

    tried D2.cpp to 'D'

--- 2019/0622_ABC131/D2.cpp
+++ 2019/0622_ABC131/D2.cpp
@@ -65,6 +65,9 @@ public:
 template <typename T>
 void my_swap(T &a, T &b)
 {
+#if DEBUG == 1
+  std::cerr << "aaaa" << std::endl;
+#endif
   T tmp{std::move(a)};
   a = std::move(b);
   b = std::move(tmp);
@@ -156,9 +159,6 @@ void vector<T, A>::reserve(size_type new_alloc)
     new (static_cast<void *>(&*oo)) T{std::move(*begin)};
     begin->~T();
   }
-#if DEBUG == 1
-  std::cerr << "aaaa" << std::endl;
-#endif
   my_swap<vector_base<T, A>>(vb, b);
 }
 
