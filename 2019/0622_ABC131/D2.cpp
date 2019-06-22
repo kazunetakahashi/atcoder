112bf16957b403c31c20f6cd7a19120d5cef873d
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-06-23 06:30:28 +0900

    tried D2.cpp to 'D'

--- 2019/0622_ABC131/D2.cpp
+++ 2019/0622_ABC131/D2.cpp
@@ -208,11 +208,11 @@ int main()
   {
     ll a, b;
     std::cin >> a >> b;
+#if DEBUG == 1
+    std::cerr << "aaa" << std::endl;
+#endif
     V.push_back(work(b, a));
   }
-#if DEBUG == 1
-  std::cerr << "aaa" << std::endl;
-#endif
   std::sort(V.begin(), V.end());
   ll now = 0;
   for (auto i = 0; i < N; i++)
