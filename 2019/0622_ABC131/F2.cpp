f563514f813ae2c276605873b892daab61ad4de0
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-06-23 00:57:38 +0900

    submit F2.cpp to 'F - Must Be Rectangular!' (abc131) [C++14 (GCC 5.4.1)]

---
+++ 2019/0622_ABC131/F2.cpp
@@ -0,0 +1,169 @@
+#define DEBUG 1
+/**
+ * File    : F2.cpp
+ * Author  : Kazune Takahashi
+ * Created : 6/23/2019, 12:52:34 AM
+ * Powered by Visual Studio Code
+ */
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <functional>
+#include <random>
+#include <chrono>
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+typedef long long ll;
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+void No()
+{
+  cout << "No" << endl;
+  exit(0);
+}
+const int MAX_SIZE = 1000010;
+class mint
+{
+public:
+  static ll MOD;
+  ll x;
+  mint() : x(0) {}
+  mint(ll x) : x(x % MOD) {}
+  mint operator-() const { return x ? MOD - x : 0; }
+  mint &operator+=(const mint &a)
+  {
+    if ((x += a.x) >= MOD)
+    {
+      x -= MOD;
+    }
+    return *this;
+  }
+  mint &operator-=(const mint &a) { return *this += -a; }
+  mint &operator*=(const mint &a)
+  {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint &operator/=(const mint &a) { return (*this *= power(MOD - 2)); }
+  mint operator+(const mint &a) const { return mint(*this) += a; }
+  mint operator-(const mint &a) const { return mint(*this) -= a; }
+  mint operator*(const mint &a) const { return mint(*this) *= a; }
+  mint operator/(const mint &a) const { return mint(*this) /= a; }
+  bool operator<(const mint &a) const { return x < a.x; }
+  bool operator==(const mint &a) const { return x == a.x; }
+  const mint power(ll N)
+  {
+    if (N == 0)
+    {
+      return 1;
+    }
+    else if (N % 2 == 1)
+    {
+      return *this * power(N - 1);
+    }
+    else
+    {
+      mint half = power(N / 2);
+      return half * half;
+    }
+  }
+};
+ll mint::MOD = 1e9 + 7;
+istream &operator>>(istream &stream, mint &a) { return stream >> a.x; }
+ostream &operator<<(ostream &stream, const mint &a) { return stream << a.x; }
+mint inv[MAX_SIZE];
+mint fact[MAX_SIZE];
+mint factinv[MAX_SIZE];
+void init()
+{
+  inv[1] = 1;
+  for (int i = 2; i < MAX_SIZE; i++)
+  {
+    inv[i] = (-inv[mint::MOD % i]) * (mint::MOD / i);
+  }
+  fact[0] = factinv[0] = 1;
+  for (int i = 1; i < MAX_SIZE; i++)
+  {
+    fact[i] = mint(i) * fact[i - 1];
+    factinv[i] = inv[i] * factinv[i - 1];
+  }
+}
+mint choose(int n, int k)
+{
+  if (n >= 0 && k >= 0 && n - k >= 0)
+  {
+    return fact[n] * factinv[k] * factinv[n - k];
+  }
+  return 0;
+}
+ll gcd(ll x, ll y) { return y ? gcd(y, x % y) : x; }
+// const double epsilon = 1e-10;
+// const ll infty = 1000000000000000LL;
+// const int dx[4] = {1, 0, -1, 0};
+// const int dy[4] = {0, 1, 0, -1};
+
+int N;
+vector<int> V[200010];
+bool visited[200010];
+ll cnt[2];
+
+void dfs(int v)
+{
+  if (visited[v])
+  {
+    return;
+  }
+  visited[v] = true;
+  cnt[v / 100005]++;
+  for (auto e : V[v])
+  {
+    dfs(e);
+  }
+}
+
+int main()
+{
+  // init();
+  cin >> N;
+  for (auto i = 0; i < N; i++)
+  {
+    int x, y;
+    cin >> x >> y;
+    y += 100005;
+    V[x].push_back(y);
+    V[y].push_back(x);
+  }
+  ll ans = 0;
+  for (auto i = 0; i < 200010; i++)
+  {
+    cnt[0] = cnt[1] = 0;
+    if (visited[i])
+    {
+      continue;
+    }
+    dfs(i);
+    ans += cnt[0] * cnt[1];
+  }
+  ans -= N;
+  cout << ans << endl;
+}
