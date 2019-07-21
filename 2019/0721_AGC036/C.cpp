87c1753dc1f6603a753ce4ca71b2d467302b8fca
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-07-21 22:53:24 +0900

    tried C.cpp to 'C'

--- 2019/0721_AGC036/C.cpp
+++ 2019/0721_AGC036/C.cpp
@@ -135,10 +135,11 @@ combination C;
 int main()
 {
   cin >> N >> M;
+  mint n{N};
   mint ans = C(3 * M + N - 1, N - 1);
   for (auto K = 2 * M + 1; K <= 3 * M; K++)
   {
-    ans -= C(3 * M - K + N - 2, N - 2) * N;
+    ans -= C(3 * M - K + N - 2, N - 2) * n;
   }
   cout << ans << endl;
 }
