be085aac3a09b6a4bf7ffc5c651a196dbe1249fc
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-08-05 00:50:11 +0900

    submit E3.cpp to 'E - Max GCD' (abc136) [C++14 (GCC 5.4.1)]

---
+++ 2019/0804_ABC136/E3.cpp
@@ -0,0 +1,175 @@
+#define DEBUG 1
+/**
+ * File    : E3.cpp
+ * Author  : Kazune Takahashi
+ * Created : 8/5/2019, 12:46:17 AM
+ * Powered by Visual Studio Code
+ */
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <functional>
+#include <random>
+#include <chrono>
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using ll = long long;
+class mint
+{
+public:
+  static ll MOD;
+  ll x;
+  mint() : x(0) {}
+  mint(ll x) : x(x % MOD) {}
+  mint operator-() const { return x ? MOD - x : 0; }
+  mint &operator+=(const mint &a)
+  {
+    if ((x += a.x) >= MOD)
+    {
+      x -= MOD;
+    }
+    return *this;
+  }
+  mint &operator-=(const mint &a) { return *this += -a; }
+  mint &operator*=(const mint &a)
+  {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint &operator/=(const mint &a)
+  {
+    mint b{a};
+    return *this *= b.power(MOD - 2);
+  }
+  mint operator+(const mint &a) const { return mint(*this) += a; }
+  mint operator-(const mint &a) const { return mint(*this) -= a; }
+  mint operator*(const mint &a) const { return mint(*this) *= a; }
+  mint operator/(const mint &a) const { return mint(*this) /= a; }
+  bool operator<(const mint &a) const { return x < a.x; }
+  bool operator==(const mint &a) const { return x == a.x; }
+  const mint power(ll N)
+  {
+    if (N == 0)
+    {
+      return 1;
+    }
+    else if (N % 2 == 1)
+    {
+      return *this * power(N - 1);
+    }
+    else
+    {
+      mint half = power(N / 2);
+      return half * half;
+    }
+  }
+};
+ll mint::MOD = 1e9 + 7;
+istream &operator>>(istream &stream, mint &a) { return stream >> a.x; }
+ostream &operator<<(ostream &stream, const mint &a) { return stream << a.x; }
+class combination
+{
+public:
+  vector<mint> inv, fact, factinv;
+  static int MAX_SIZE;
+  combination() : inv(MAX_SIZE), fact(MAX_SIZE), factinv(MAX_SIZE)
+  {
+    inv[1] = 1;
+    for (auto i = 2; i < MAX_SIZE; i++)
+    {
+      inv[i] = (-inv[mint::MOD % i]) * (mint::MOD / i);
+    }
+    fact[0] = factinv[0] = 1;
+    for (auto i = 1; i < MAX_SIZE; i++)
+    {
+      fact[i] = mint(i) * fact[i - 1];
+      factinv[i] = inv[i] * factinv[i - 1];
+    }
+  }
+  mint operator()(int n, int k)
+  {
+    if (n >= 0 && k >= 0 && n - k >= 0)
+    {
+      return fact[n] * factinv[k] * factinv[n - k];
+    }
+    return 0;
+  }
+};
+int combination::MAX_SIZE = 3000010;
+ll gcd(ll x, ll y) { return y ? gcd(y, x % y) : x; }
+// constexpr double epsilon = 1e-10;
+// constexpr ll infty = 1000000000000000LL;
+// constexpr int dx[4] = {1, 0, -1, 0};
+// constexpr int dy[4] = {0, 1, 0, -1};
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+void No()
+{
+  cout << "No" << endl;
+  exit(0);
+}
+
+ll N, K;
+vector<ll> A;
+
+bool solve(ll X)
+{
+  vector<ll> R(N);
+  for (auto i = 0; i < N; i++)
+  {
+    R[i] = A[i] % X;
+  }
+  sort(R.begin(), R.end());
+  ll total = accumulate(R.begin(), R.end(), 0LL);
+  int L = N - total / X;
+  ll need = accumulate(R.begin(), R.begin() + L, 0LL);
+  return need <= K;
+}
+
+int main()
+{
+  cin >> N >> K;
+  A.resize(N);
+  for (auto i = 0; i < N; i++)
+  {
+    cin >> A[i];
+  }
+  ll sum = accumulate(A.begin(), A.end(), 0LL);
+  set<ll> candidates;
+  for (auto i = 1; i * i <= sum; i++)
+  {
+    if (sum % i == 0)
+    {
+      candidates.insert(i);
+      candidates.insert(sum / i);
+    }
+  }
+  for (auto it = candidates.rbegin(); it != candidates.rend(); it++)
+  {
+    if (solve(*it))
+    {
+      cout << *it << endl;
+      return 0;
+    }
+  }
+}
