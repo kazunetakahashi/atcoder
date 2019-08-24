ea5beaa7222a1b03685fee5bc9620daaaab25db8
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-08-24 22:46:02 +0900

    tried E.cpp to 'E'

---
+++ 2019/0824_jsc2019-qual/E.cpp
@@ -0,0 +1,222 @@
+#define DEBUG 1
+/**
+ * File    : E.cpp
+ * Author  : Kazune Takahashi
+ * Created : 8/24/2019, 10:33:10 PM
+ * Powered by Visual Studio Code
+ */
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <functional>
+#include <random>
+#include <chrono>
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+#define maxs(x, y) (x = max(x, y))
+#define mins(x, y) (x = min(x, y))
+using ll = long long;
+class mint
+{
+public:
+  static ll MOD;
+  ll x;
+  mint() : x(0) {}
+  mint(ll x) : x(x % MOD) {}
+  mint operator-() const { return x ? MOD - x : 0; }
+  mint &operator+=(const mint &a)
+  {
+    if ((x += a.x) >= MOD)
+    {
+      x -= MOD;
+    }
+    return *this;
+  }
+  mint &operator-=(const mint &a) { return *this += -a; }
+  mint &operator*=(const mint &a)
+  {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint &operator/=(const mint &a)
+  {
+    mint b{a};
+    return *this *= b.power(MOD - 2);
+  }
+  mint operator+(const mint &a) const { return mint(*this) += a; }
+  mint operator-(const mint &a) const { return mint(*this) -= a; }
+  mint operator*(const mint &a) const { return mint(*this) *= a; }
+  mint operator/(const mint &a) const { return mint(*this) /= a; }
+  bool operator<(const mint &a) const { return x < a.x; }
+  bool operator==(const mint &a) const { return x == a.x; }
+  const mint power(ll N)
+  {
+    if (N == 0)
+    {
+      return 1;
+    }
+    else if (N % 2 == 1)
+    {
+      return *this * power(N - 1);
+    }
+    else
+    {
+      mint half = power(N / 2);
+      return half * half;
+    }
+  }
+};
+ll mint::MOD = 1e9 + 7;
+istream &operator>>(istream &stream, mint &a) { return stream >> a.x; }
+ostream &operator<<(ostream &stream, const mint &a) { return stream << a.x; }
+class combination
+{
+public:
+  vector<mint> inv, fact, factinv;
+  static int MAX_SIZE;
+  combination() : inv(MAX_SIZE), fact(MAX_SIZE), factinv(MAX_SIZE)
+  {
+    inv[1] = 1;
+    for (auto i = 2; i < MAX_SIZE; i++)
+    {
+      inv[i] = (-inv[mint::MOD % i]) * (mint::MOD / i);
+    }
+    fact[0] = factinv[0] = 1;
+    for (auto i = 1; i < MAX_SIZE; i++)
+    {
+      fact[i] = mint(i) * fact[i - 1];
+      factinv[i] = inv[i] * factinv[i - 1];
+    }
+  }
+  mint operator()(int n, int k)
+  {
+    if (n >= 0 && k >= 0 && n - k >= 0)
+    {
+      return fact[n] * factinv[k] * factinv[n - k];
+    }
+    return 0;
+  }
+};
+int combination::MAX_SIZE = 3000010;
+ll gcd(ll x, ll y) { return y ? gcd(y, x % y) : x; }
+// constexpr double epsilon = 1e-10;
+// constexpr ll infty = 1000000000000000LL;
+// constexpr int dx[4] = {1, 0, -1, 0};
+// constexpr int dy[4] = {0, 1, 0, -1};
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+void No()
+{
+  cout << "No" << endl;
+  exit(0);
+}
+
+class UnionFind
+{
+  vector<long long> par;
+
+public:
+  UnionFind() {}
+  UnionFind(int n) : par(n, -1) {}
+
+  bool is_same(int x, int y)
+  {
+    return root(x) == root(y);
+  }
+
+  bool merge(int x, int y)
+  {
+    x = root(x);
+    y = root(y);
+    if (x == y)
+    {
+      return false;
+    }
+    if (par[x] > par[y])
+    {
+      swap(x, y);
+    }
+    par[x] += par[y];
+    par[y] = x;
+    return true;
+  }
+
+  long long size(int x)
+  {
+    return -par[root(x)];
+  }
+
+  int root(int x)
+  {
+    if (par[x] < 0)
+    {
+      return x;
+    }
+    return par[x] = root(par[x]);
+  }
+};
+
+int N, H, W;
+using info = tuple<ll, ll, ll>;
+vector<info> V;
+vector<info> X;
+
+int main()
+{
+  cin >> N >> H >> W;
+  UnionFind UF(H + W);
+  for (auto i = 0; i < N; i++)
+  {
+    ll R, C, A;
+    cin >> R >> C >> A;
+    R--;
+    C--;
+    V.emplace_back(A, R, H + C);
+  }
+  sort(V.rbegin(), V.rend());
+  ll ans{0};
+  for (auto i = 0; i < N; i++)
+  {
+    ll R, C, A;
+    tie(A, R, C) = V[i];
+    if (!UF.is_same(R, C))
+    {
+      ans += A;
+      UF.merge(R, C);
+    }
+    else
+    {
+      X.push_back(V[i]);
+    }
+  }
+  vector<bool> used(H + W, false);
+  for (auto x : X)
+  {
+    ll R, C, A;
+    tie(A, R, C) = x;
+    if (!used[UF.root(R)])
+    {
+      used[UF.root(R)] = true;
+      ans += A;
+    }
+  }
+  cout << ans << endl;
+}
