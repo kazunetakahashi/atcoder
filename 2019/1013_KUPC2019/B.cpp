12d99ff69cb0358f1645433489ace1411dc897d1
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-10-13 13:21:47 +0900

    tried B.cpp to 'B'

---
+++ 2019/1013_KUPC2019/B.cpp
@@ -0,0 +1,217 @@
+#define DEBUG 1
+/**
+ * File    : B.cpp
+ * Author  : Kazune Takahashi
+ * Created : 10/13/2019, 1:10:38 PM
+ * Powered by Visual Studio Code
+ */
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <functional>
+#include <random>
+#include <chrono>
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+template <typename T>
+void ch_max(T &left, T right)
+{
+  if (left < right)
+  {
+    left = right;
+  }
+}
+template <typename T>
+void ch_min(T &left, T right)
+{
+  if (left > right)
+  {
+    left = right;
+  }
+}
+using ll = long long;
+class mint
+{
+public:
+  static ll MOD;
+  ll x;
+  mint() : x(0) {}
+  mint(ll x) : x(x % MOD) {}
+  mint operator-() const { return x ? MOD - x : 0; }
+  mint &operator+=(const mint &a)
+  {
+    if ((x += a.x) >= MOD)
+    {
+      x -= MOD;
+    }
+    return *this;
+  }
+  mint &operator-=(const mint &a) { return *this += -a; }
+  mint &operator*=(const mint &a)
+  {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint &operator/=(const mint &a)
+  {
+    mint b{a};
+    return *this *= b.power(MOD - 2);
+  }
+  mint operator+(const mint &a) const { return mint(*this) += a; }
+  mint operator-(const mint &a) const { return mint(*this) -= a; }
+  mint operator*(const mint &a) const { return mint(*this) *= a; }
+  mint operator/(const mint &a) const { return mint(*this) /= a; }
+  bool operator<(const mint &a) const { return x < a.x; }
+  bool operator==(const mint &a) const { return x == a.x; }
+  const mint power(ll N)
+  {
+    if (N == 0)
+    {
+      return 1;
+    }
+    else if (N % 2 == 1)
+    {
+      return *this * power(N - 1);
+    }
+    else
+    {
+      mint half = power(N / 2);
+      return half * half;
+    }
+  }
+};
+ll mint::MOD = 1e9 + 7;
+istream &operator>>(istream &stream, mint &a) { return stream >> a.x; }
+ostream &operator<<(ostream &stream, const mint &a) { return stream << a.x; }
+class combination
+{
+public:
+  vector<mint> inv, fact, factinv;
+  static int MAX_SIZE;
+  combination() : inv(MAX_SIZE), fact(MAX_SIZE), factinv(MAX_SIZE)
+  {
+    inv[1] = 1;
+    for (auto i = 2; i < MAX_SIZE; i++)
+    {
+      inv[i] = (-inv[mint::MOD % i]) * (mint::MOD / i);
+    }
+    fact[0] = factinv[0] = 1;
+    for (auto i = 1; i < MAX_SIZE; i++)
+    {
+      fact[i] = mint(i) * fact[i - 1];
+      factinv[i] = inv[i] * factinv[i - 1];
+    }
+  }
+  mint operator()(int n, int k)
+  {
+    if (n >= 0 && k >= 0 && n - k >= 0)
+    {
+      return fact[n] * factinv[k] * factinv[n - k];
+    }
+    return 0;
+  }
+};
+int combination::MAX_SIZE = 3000010;
+ll gcd(ll x, ll y) { return y ? gcd(y, x % y) : x; }
+// constexpr double epsilon = 1e-10;
+// constexpr ll infty = 1000000000000000LL;
+// constexpr int dx[4] = {1, 0, -1, 0};
+// constexpr int dy[4] = {0, 1, 0, -1};
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+void No()
+{
+  cout << "No" << endl;
+  exit(0);
+}
+
+int main()
+{
+  int N, M, A;
+  cin >> N >> M >> A;
+  vector<int> W(N), V(N);
+  for (auto i = 0; i < N; i++)
+  {
+    cin >> W[i] >> V[i];
+  }
+  vector<vector<int>> T(N);
+  for (auto i = 0; i < M; i++)
+  {
+    int a, b;
+    cin >> a >> b;
+    --a;
+    --b;
+    T[a].push_back(b);
+    T[b].push_back(a);
+  }
+  vector<bool> visited(N, false);
+  using info = tuple<int, int>;
+  vector<info> K;
+  for (auto i = 0; i < N; i++)
+  {
+    if (!visited[i])
+    {
+      stack<int> S;
+      S.push(i);
+      vector<int> X;
+      while (!S.empty())
+      {
+        int now = S.top();
+        S.pop();
+        if (!visited[now])
+        {
+          visited[now] = true;
+          X.push_back(now);
+          for (auto x : T[now])
+          {
+            if (!visited[x])
+            {
+              S.push(x);
+            }
+          }
+        }
+      }
+      int v{0}, w{0};
+      for (auto x : X)
+      {
+        v += V[x];
+        w += W[x];
+      }
+      K.push_back(info(v, w));
+    }
+  }
+  vector<int> DP(A, -1);
+  DP[0] = 0;
+  for (auto f : K)
+  {
+    int v{get<0>(f)};
+    int w{get<1>(f)};
+    for (auto i = A - 1; i >= 0; i--)
+    {
+      if (DP[i] == -1 || i + w >= A)
+      {
+        continue;
+      }
+      ch_max(DP[i + w], DP[i] + v);
+    }
+  }
+  cout << *max_element(DP.begin(), DP.end()) << endl;
+}
