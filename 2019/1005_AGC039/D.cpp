d6bf9c4186042fefdc8cc16201c12a45a34c01b6
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-10-18 20:02:23 +0900

    tried D.cpp to 'D'

---
+++ 2019/1005_AGC039/D.cpp
@@ -0,0 +1,183 @@
+#define DEBUG 1
+/**
+ * File    : D.cpp
+ * Author  : Kazune Takahashi
+ * Created : 10/18/2019, 7:51:02 PM
+ * Powered by Visual Studio Code
+ */
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <functional>
+#include <random>
+#include <chrono>
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+template <typename T>
+void ch_max(T &left, T right)
+{
+  if (left < right)
+  {
+    left = right;
+  }
+}
+template <typename T>
+void ch_min(T &left, T right)
+{
+  if (left > right)
+  {
+    left = right;
+  }
+}
+using ll = long long;
+class mint
+{
+public:
+  static ll MOD;
+  ll x;
+  mint() : x(0) {}
+  mint(ll x) : x(x % MOD) {}
+  mint operator-() const { return x ? MOD - x : 0; }
+  mint &operator+=(const mint &a)
+  {
+    if ((x += a.x) >= MOD)
+    {
+      x -= MOD;
+    }
+    return *this;
+  }
+  mint &operator-=(const mint &a) { return *this += -a; }
+  mint &operator*=(const mint &a)
+  {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  mint &operator/=(const mint &a)
+  {
+    mint b{a};
+    return *this *= b.power(MOD - 2);
+  }
+  mint operator+(const mint &a) const { return mint(*this) += a; }
+  mint operator-(const mint &a) const { return mint(*this) -= a; }
+  mint operator*(const mint &a) const { return mint(*this) *= a; }
+  mint operator/(const mint &a) const { return mint(*this) /= a; }
+  bool operator<(const mint &a) const { return x < a.x; }
+  bool operator==(const mint &a) const { return x == a.x; }
+  const mint power(ll N)
+  {
+    if (N == 0)
+    {
+      return 1;
+    }
+    else if (N % 2 == 1)
+    {
+      return *this * power(N - 1);
+    }
+    else
+    {
+      mint half = power(N / 2);
+      return half * half;
+    }
+  }
+};
+ll mint::MOD = 1e9 + 7;
+istream &operator>>(istream &stream, mint &a) { return stream >> a.x; }
+ostream &operator<<(ostream &stream, const mint &a) { return stream << a.x; }
+class combination
+{
+public:
+  vector<mint> inv, fact, factinv;
+  static int MAX_SIZE;
+  combination() : inv(MAX_SIZE), fact(MAX_SIZE), factinv(MAX_SIZE)
+  {
+    inv[1] = 1;
+    for (auto i = 2; i < MAX_SIZE; i++)
+    {
+      inv[i] = (-inv[mint::MOD % i]) * (mint::MOD / i);
+    }
+    fact[0] = factinv[0] = 1;
+    for (auto i = 1; i < MAX_SIZE; i++)
+    {
+      fact[i] = mint(i) * fact[i - 1];
+      factinv[i] = inv[i] * factinv[i - 1];
+    }
+  }
+  mint operator()(int n, int k)
+  {
+    if (n >= 0 && k >= 0 && n - k >= 0)
+    {
+      return fact[n] * factinv[k] * factinv[n - k];
+    }
+    return 0;
+  }
+};
+int combination::MAX_SIZE = 3000010;
+ll gcd(ll x, ll y) { return y ? gcd(y, x % y) : x; }
+// constexpr double epsilon = 1e-10;
+// constexpr ll infty = 1000000000000000LL;
+// constexpr int dx[4] = {1, 0, -1, 0};
+// constexpr int dy[4] = {0, 1, 0, -1};
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+void No()
+{
+  cout << "No" << endl;
+  exit(0);
+}
+
+ll N, L;
+constexpr double pi{M_PI};
+
+double X(double t)
+{
+  return cos(2 * pi * t / L);
+}
+
+double Y(double t)
+{
+  return sin(2 * pi * t / L);
+}
+
+int main()
+{
+  cin >> N >> L;
+  vector<double> T(N);
+  for (auto i = 0; i < N; i++)
+  {
+    cin >> T[i];
+  }
+  double sum_X{0.0}, sum_Y{0.0};
+  for (auto i = 0; i < N; i++)
+  {
+    for (auto j = i + 1; j < N; j++)
+    {
+      double h{(T[i] + T[j]) / 2};
+      ll cnt{N - j - i - 1 - 2};
+      sum_X += X(h) * cnt;
+      sum_Y += Y(h) * cnt;
+      ll cnt_2{N - 2 - cnt};
+      sum_X += X(h + pi) * cnt_2;
+      sum_Y += Y(h + pi) * cnt_2;
+    }
+  }
+  ll C{N * (N - 1) * (N - 2) / 6};
+  cout << fixed << setprecision(15) << sum_X / C << " " << sum_Y / C << endl;
+}
