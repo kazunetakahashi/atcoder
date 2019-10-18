00e20ad5b3ef9121c5ca5029f16b4f34d1e56684
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-10-18 20:06:32 +0900

    tried D.cpp to 'D'

--- 2019/1005_AGC039/D.cpp
+++ 2019/1005_AGC039/D.cpp
@@ -171,6 +171,9 @@ int main()
     {
       double h{(T[i] + T[j]) / 2};
       ll cnt{N - (j - i - 1) - 2};
+#if DEBUG == 1
+      cerr << "cnt = " << cnt << ", h = " << h << endl;
+#endif
       sum_X += X(h) * cnt;
       sum_Y += Y(h) * cnt;
       ll cnt_2{N - 2 - cnt};
