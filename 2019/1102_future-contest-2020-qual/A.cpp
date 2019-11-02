39e7395a183342dc80f8a477666e2a213dc51e6f
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-11-02 17:31:13 +0900

    submit A.cpp to 'A - ロボットの誘導' (future-contest-2020-qual) [C++14 (GCC 5.4.1)]

--- 2019/1102_future-contest-2020-qual/A.cpp
+++ 2019/1102_future-contest-2020-qual/A.cpp
@@ -49,6 +49,23 @@ void TLE()
   }
 }
 
+namespace Tools
+{
+
+template <typename T>
+T get(Point const &p, vector<vector<T>> const &X)
+{
+  return X[get<0>(p)][get<1>(p)];
+}
+
+template <typename T>
+void set(Point const &p, T val, vector<vector<T>> &X)
+{
+  X[get<0>(p)][get<1>(p)] = val;
+}
+
+}; // namespace Tools
+
 enum class State
 {
   Empty,
@@ -62,38 +79,27 @@ class Field
 {
 public:
   static int N;
-  int M, B;
   static vector<Point> V;
   static vector<vector<Point>> P;
+  int M, B;
   vector<vector<int>> D;
   vector<vector<State>> S;
+  vector<Point> robots;
   bool is_sample_one;
 
   Field();
   void solve();
   void flush();
+  int score();
+  int direction_count();
 
-  State get_state(Point const &p) const
-  {
-    return S[get<0>(p)][get<1>(p)];
-  }
-
-  State &set_state(Point const &p, State s)
-  {
-    return S[get<0>(p)][get<1>(p)] = s;
-  }
-
-  int get_direction(Point const &p) const
-  {
-    return D[get<0>(p)][get<1>(p)];
-  }
-
-  int &set_direction(Point const &p, int d)
-  {
-    return D[get<0>(p)][get<1>(p)] = d;
-  }
+  State get_state(Point const &p) const { return Tools::get(p, S); }
+  void set_state(Point const &p, State s) { Tools::set(p, s, S); }
+  int get_direction(Point const &p) const { return Tools::get(p, D); }
+  void set_direction(Point const &p, int d) { Tools::set(p, d, D); }
 
 private:
+  bool reach(Point const &p, vector<vector<bool>> &used);
   void good_place();
   void other_place();
   void fill_empty();
@@ -116,6 +122,7 @@ Field::Field()
   {
     cin >> x >> y >> c;
     S[x][y] = State::Robot;
+    robots.emplace_back(x, y);
   }
   for (auto i = 0; i < B; i++)
   {
@@ -123,12 +130,12 @@ Field::Field()
     S[x][y] = State::Block;
   }
   S[gx][gy] = State::Goal;
-  is_sample_one = (gx == 32 && gy == 8);
+  is_sample_one = (gx == 32 && gy == 8); // これで十分のようだ。
 }
 
 void Field::solve()
 {
-  if (is_sample_one)
+  if (!is_sample_one)
   {
     good_place();
     other_place();
@@ -136,7 +143,9 @@ void Field::solve()
   }
   else
   {
-    TLE();
+    good_place();
+    other_place();
+    fill_empty();
   }
 }
 
@@ -175,7 +184,6 @@ void Field::other_place()
         {
           Point p{P[i][j] + V[k]};
           int d{get_direction(p)};
-          // State s{get_state(p)};
           if (d != -1)
           {
             D[i][j] = k;
@@ -189,7 +197,6 @@ void Field::other_place()
         for (auto k = 0; k < 4; k++)
         {
           Point p{P[i][j] + V[k]};
-          // int d{get_direction(p)};
           State s{get_state(p)};
           if (s == State::Empty)
           {
@@ -221,25 +228,31 @@ void Field::fill_empty()
   }
 }
 
-void Field::flush()
+int Field::direction_count()
 {
   int K{0};
   for (auto x = 0; x < N; x++)
   {
     for (auto y = 0; y < N; y++)
     {
-      if (0 <= D[x][y] && D[x][y] < 4)
+      if (S[x][y] != State::Goal && S[x][y] != State::Block && 0 <= D[x][y] && D[x][y] < 4)
       {
         ++K;
       }
     }
   }
+  return K;
+}
+
+void Field::flush()
+{
+  int K{Field::direction_count()};
   cout << K << endl;
   for (auto x = 0; x < N; x++)
   {
     for (auto y = 0; y < N; y++)
     {
-      if (0 <= D[x][y] && D[x][y] < 4)
+      if (S[x][y] != State::Goal && S[x][y] != State::Block && 0 <= D[x][y] && D[x][y] < 4)
       {
         cout << x << " " << y << " " << direction[D[x][y]] << endl;
       }
@@ -247,12 +260,67 @@ void Field::flush()
   }
 }
 
+bool Field::reach(Point const &p, vector<vector<bool>> &used)
+{
+  stack<Point> st;
+  vector<vector<bool>> visited(N, vector<bool>(N, false));
+  st.push(p);
+  while (!st.empty())
+  {
+    Point p{st.top()};
+    st.pop();
+    if (!Tools::get(p, visited))
+    {
+      Tools::set(p, true, visited);
+      Tools::set(p, true, used);
+      if (Tools::get(p, S) == State::Goal)
+      {
+        return true;
+      }
+      int k{get_direction(p)};
+      assert(k != -1);
+      Point q{p + V[k]};
+      if (get_state(q) == State::Block)
+      {
+        return false;
+      }
+      st.push(q);
+    }
+  }
+  return false;
+}
+
+int Field::score()
+{
+  int ans{0};
+  vector<vector<bool>> used(N, vector<bool>(N, false));
+  for (auto const &x : robots)
+  {
+    if (reach(x, used))
+    {
+      ans += 1000;
+    }
+  }
+  for (auto i = 0; i < N; i++)
+  {
+    for (auto j = 0; j < N; j++)
+    {
+      if (used[i][j])
+      {
+        ++ans;
+      }
+    }
+  }
+  ans -= 10 * direction_count();
+  return ans;
+}
+
 int main()
 {
   cin >> Field::N;
   for (auto i = 0; i < 4; i++)
   {
-    Field::V.push_back(Point{dy[i], dx[i]});
+    Field::V.push_back(Point{dx[i], dy[i]});
   }
   Field::P = vector<vector<Point>>(Field::N, vector<Point>(Field::N));
   for (auto i = 0; i < Field::N; i++)
@@ -264,5 +332,6 @@ int main()
   }
   Field f;
   f.solve();
+  cerr << f.score() << endl;
   f.flush();
 }
