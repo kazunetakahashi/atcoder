72c2ec1af0fa5cefb26fd5c3af9b412d0a8f197e
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-11-06 13:51:36 +0900

    submit A2.cpp to 'A - ><' (agc040) [C++14 (GCC 5.4.1)]

---
+++ 2019/1103_AGC040/A2.cpp
@@ -0,0 +1,203 @@
+#define DEBUG 1
+/**
+ * File    : A2.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2019/11/6 13:47:31
+ * Powered by Visual Studio Code
+ */
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <functional>
+#include <random>
+#include <chrono>
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+template <typename T>
+void ch_max(T &left, T right)
+{
+  if (left < right)
+  {
+    left = right;
+  }
+}
+template <typename T>
+void ch_min(T &left, T right)
+{
+  if (left > right)
+  {
+    left = right;
+  }
+}
+using ll = long long;
+constexpr ll MOD{1000000007LL};
+constexpr ll MAX_SIZE{3000010LL};
+template <ll MOD = MOD>
+class Mint
+{
+public:
+  ll x;
+  Mint() : x{0LL} {}
+  Mint(ll x) : x{x % MOD} {}
+  Mint operator-() const { return x ? MOD - x : 0; }
+  Mint &operator+=(const Mint &a)
+  {
+    if ((x += a.x) >= MOD)
+    {
+      x -= MOD;
+    }
+    return *this;
+  }
+  Mint &operator-=(const Mint &a) { return *this += -a; }
+  Mint &operator*=(const Mint &a)
+  {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  Mint &operator/=(const Mint &a)
+  {
+    Mint b{a};
+    return *this *= b.power(MOD - 2);
+  }
+  Mint operator+(const Mint &a) const { return Mint(*this) += a; }
+  Mint operator-(const Mint &a) const { return Mint(*this) -= a; }
+  Mint operator*(const Mint &a) const { return Mint(*this) *= a; }
+  Mint operator/(const Mint &a) const { return Mint(*this) /= a; }
+  bool operator<(const Mint &a) const { return x < a.x; }
+  bool operator==(const Mint &a) const { return x == a.x; }
+  const Mint power(ll N)
+  {
+    if (N == 0)
+    {
+      return 1;
+    }
+    else if (N % 2 == 1)
+    {
+      return *this * power(N - 1);
+    }
+    else
+    {
+      Mint half = power(N / 2);
+      return half * half;
+    }
+  }
+};
+template <ll MOD>
+Mint<MOD> operator+(ll lhs, const Mint<MOD> &rhs)
+{
+  return rhs + lhs;
+}
+template <ll MOD>
+Mint<MOD> operator-(ll lhs, const Mint<MOD> &rhs)
+{
+  return -rhs + lhs;
+}
+template <ll MOD>
+Mint<MOD> operator*(ll lhs, const Mint<MOD> &rhs)
+{
+  return rhs * lhs;
+}
+template <ll MOD>
+Mint<MOD> operator/(ll lhs, const Mint<MOD> &rhs)
+{
+  return Mint<MOD>{lhs} / rhs;
+}
+template <ll MOD>
+istream &operator>>(istream &stream, Mint<MOD> &a)
+{
+  return stream >> a.x;
+}
+template <ll MOD>
+ostream &operator<<(ostream &stream, const Mint<MOD> &a)
+{
+  return stream << a.x;
+}
+template <ll MOD = MOD, ll MAX_SIZE = MAX_SIZE>
+class Combination
+{
+public:
+  vector<Mint<MOD>> inv, fact, factinv;
+  Combination() : inv(MAX_SIZE), fact(MAX_SIZE), factinv(MAX_SIZE)
+  {
+    inv[1] = 1;
+    for (auto i = 2LL; i < MAX_SIZE; i++)
+    {
+      inv[i] = (-inv[MOD % i]) * (MOD / i);
+    }
+    fact[0] = factinv[0] = 1;
+    for (auto i = 1LL; i < MAX_SIZE; i++)
+    {
+      fact[i] = Mint<MOD>(i) * fact[i - 1];
+      factinv[i] = inv[i] * factinv[i - 1];
+    }
+  }
+  Mint<MOD> operator()(int n, int k)
+  {
+    if (n >= 0 && k >= 0 && n - k >= 0)
+    {
+      return fact[n] * factinv[k] * factinv[n - k];
+    }
+    return 0;
+  }
+  Mint<MOD> catalan(int x, int y)
+  {
+    return (*this)(x + y, y) - (*this)(x + y, y - 1);
+  }
+};
+// for C++14
+using mint = Mint<MOD>;
+using combination = Combination<MOD, MAX_SIZE>;
+ll gcd(ll x, ll y) { return y ? gcd(y, x % y) : x; }
+// constexpr double epsilon = 1e-10;
+// constexpr ll infty = 1000000000000000LL;
+// constexpr int dx[4] = {1, 0, -1, 0};
+// constexpr int dy[4] = {0, 1, 0, -1};
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+void No()
+{
+  cout << "No" << endl;
+  exit(0);
+}
+
+void solve(string const &S, int N, vector<ll> &V, char x)
+{
+  for (auto i = 0; i < N - 1; i++)
+  {
+    if (S[i] == x)
+    {
+      ch_max(V[i + 1], V[i] + 1);
+    }
+  }
+}
+
+int main()
+{
+  string S;
+  cin >> S;
+  int N(S.size() + 1);
+  vector<ll> V(N, 0LL);
+  solve(S, N, V, '<');
+  reverse(V.begin(), V.end());
+  reverse(S.begin(), S.end());
+  solve(S, N, V, '>');
+  cout << accumulate(V.begin(), V.end(), 0LL) << endl;
+}
