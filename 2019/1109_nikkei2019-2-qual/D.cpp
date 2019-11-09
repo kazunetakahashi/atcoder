50a027ced6fdccecdf3507d036716a6d52fa0c31
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-11-09 23:26:23 +0900

    tried D.cpp to 'D'

---
+++ 2019/1109_nikkei2019-2-qual/D.cpp
@@ -0,0 +1,290 @@
+#define DEBUG 1
+/**
+ * File    : D.cpp
+ * Author  : Kazune Takahashi
+ * Created : 11/9/2019, 10:48:55 PM
+ * Powered by Visual Studio Code
+ */
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <complex>
+#include <tuple>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <functional>
+#include <random>
+#include <chrono>
+#include <cctype>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+template <typename T>
+void ch_max(T &left, T right)
+{
+  if (left < right)
+  {
+    left = right;
+  }
+}
+template <typename T>
+void ch_min(T &left, T right)
+{
+  if (left > right)
+  {
+    left = right;
+  }
+}
+using ll = long long;
+constexpr ll MOD{1000000007LL};
+constexpr ll MAX_SIZE{3000010LL};
+template <ll MOD = MOD>
+class Mint
+{
+public:
+  ll x;
+  Mint() : x{0LL} {}
+  Mint(ll x) : x{x % MOD} {}
+  Mint operator-() const { return x ? MOD - x : 0; }
+  Mint &operator+=(const Mint &a)
+  {
+    if ((x += a.x) >= MOD)
+    {
+      x -= MOD;
+    }
+    return *this;
+  }
+  Mint &operator-=(const Mint &a) { return *this += -a; }
+  Mint &operator*=(const Mint &a)
+  {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  Mint &operator/=(const Mint &a)
+  {
+    Mint b{a};
+    return *this *= b.power(MOD - 2);
+  }
+  Mint operator+(const Mint &a) const { return Mint(*this) += a; }
+  Mint operator-(const Mint &a) const { return Mint(*this) -= a; }
+  Mint operator*(const Mint &a) const { return Mint(*this) *= a; }
+  Mint operator/(const Mint &a) const { return Mint(*this) /= a; }
+  bool operator<(const Mint &a) const { return x < a.x; }
+  bool operator==(const Mint &a) const { return x == a.x; }
+  const Mint power(ll N)
+  {
+    if (N == 0)
+    {
+      return 1;
+    }
+    else if (N % 2 == 1)
+    {
+      return *this * power(N - 1);
+    }
+    else
+    {
+      Mint half = power(N / 2);
+      return half * half;
+    }
+  }
+};
+template <ll MOD>
+Mint<MOD> operator+(ll lhs, const Mint<MOD> &rhs)
+{
+  return rhs + lhs;
+}
+template <ll MOD>
+Mint<MOD> operator-(ll lhs, const Mint<MOD> &rhs)
+{
+  return -rhs + lhs;
+}
+template <ll MOD>
+Mint<MOD> operator*(ll lhs, const Mint<MOD> &rhs)
+{
+  return rhs * lhs;
+}
+template <ll MOD>
+Mint<MOD> operator/(ll lhs, const Mint<MOD> &rhs)
+{
+  return Mint<MOD>{lhs} / rhs;
+}
+template <ll MOD>
+istream &operator>>(istream &stream, Mint<MOD> &a)
+{
+  return stream >> a.x;
+}
+template <ll MOD>
+ostream &operator<<(ostream &stream, const Mint<MOD> &a)
+{
+  return stream << a.x;
+}
+template <ll MOD = MOD, ll MAX_SIZE = MAX_SIZE>
+class Combination
+{
+public:
+  vector<Mint<MOD>> inv, fact, factinv;
+  Combination() : inv(MAX_SIZE), fact(MAX_SIZE), factinv(MAX_SIZE)
+  {
+    inv[1] = 1;
+    for (auto i = 2LL; i < MAX_SIZE; i++)
+    {
+      inv[i] = (-inv[MOD % i]) * (MOD / i);
+    }
+    fact[0] = factinv[0] = 1;
+    for (auto i = 1LL; i < MAX_SIZE; i++)
+    {
+      fact[i] = Mint<MOD>(i) * fact[i - 1];
+      factinv[i] = inv[i] * factinv[i - 1];
+    }
+  }
+  Mint<MOD> operator()(int n, int k)
+  {
+    if (n >= 0 && k >= 0 && n - k >= 0)
+    {
+      return fact[n] * factinv[k] * factinv[n - k];
+    }
+    return 0;
+  }
+  Mint<MOD> catalan(int x, int y)
+  {
+    return (*this)(x + y, y) - (*this)(x + y, y - 1);
+  }
+};
+// for C++14
+using mint = Mint<MOD>;
+using combination = Combination<MOD, MAX_SIZE>;
+ll gcd(ll x, ll y) { return y ? gcd(y, x % y) : x; }
+// constexpr double epsilon = 1e-10;
+constexpr ll infty = 1000000000000000LL;
+// constexpr int dx[4] = {1, 0, -1, 0};
+// constexpr int dy[4] = {0, 1, 0, -1};
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+void No()
+{
+  cout << "-1" << endl;
+  exit(0);
+}
+
+template <typename T>
+class SegTree
+{ // 0-indexed, [0, N).
+private:
+  int N;
+  vector<T> dat;
+  T unit;  // モノイドの単位元
+  T(*func) // モノイドの演算
+  (T, T);
+  T(*_update) // update で値をどうするか書く
+  (T, T);
+
+public:
+  SegTree() {}
+
+  SegTree(int n, T unit, T (*func)(T, T), T (*_update)(T, T)) : N{1}, unit{unit}, func{func}, _update{_update}
+  {
+    while (N < n)
+    {
+      N *= 2;
+    }
+    dat = vector<T>(2 * N - 1, unit);
+  }
+
+  void update(int k, T a)
+  {
+    k += N - 1;
+    dat[k] = _update(dat[k], a);
+    while (k > 0)
+    {
+      k = (k - 1) / 2;
+      dat[k] = func(dat[k * 2 + 1], dat[k * 2 + 2]);
+    }
+  }
+
+private:
+  T find(int a, int b, int k, int l, int r)
+  {
+    if (r <= a || b <= l)
+    {
+      return unit;
+    }
+    if (a <= l && r <= b)
+    {
+      return dat[k];
+    }
+    T vl = find(a, b, k * 2 + 1, l, (l + r) / 2);
+    T vr = find(a, b, k * 2 + 2, (l + r) / 2, r);
+    return func(vl, vr);
+  }
+
+public:
+  T find(int a, int b)
+  { // [a, b) の find をする。
+    return find(a, b, 0, 0, N);
+  }
+};
+
+using Info = tuple<int, int, ll>;
+
+int main()
+{
+  int N, M;
+  cin >> N >> M;
+  vector<Info> I(M);
+  for (auto i = 0; i < M; i++)
+  {
+    int l, r;
+    l--;
+    r--;
+    ll c;
+    cin >> l >> r >> c;
+    I[i] = Info(l, r, c);
+  }
+  sort(I.begin(), I.end());
+  auto func = [](auto x, auto y) {
+    return min(x, y);
+  };
+  auto _update = [](auto x, auto y) {
+    return min(x, y);
+  };
+  constexpr ll unit{1LL << 60};
+  SegTree<ll> tree{N, unit, func, _update};
+  tree.update(0, 0);
+  int now{0};
+  for (auto i = 0; i < M; i++)
+  {
+    int l, r;
+    ll c;
+    tie(l, r, c) = I[i];
+    if (now < l)
+    {
+      No();
+    }
+    ch_max(now, r);
+  }
+  if (now < N - 1)
+  {
+    No();
+  }
+  for (auto i = 0; i < M; i++)
+  {
+    int l, r;
+    ll c;
+    tie(l, r, c) = I[i];
+    ll mini{tree.find(0, l + 1)};
+    tree.update(r, mini + c);
+  }
+  cout << tree.find(N - 1, N) << endl;
+}
