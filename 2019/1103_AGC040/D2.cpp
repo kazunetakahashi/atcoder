7128b452c88b591707af45c052a0239a74c9ae73
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-11-13 21:12:33 +0900

    tried D2.cpp to 'D'

--- 2019/1103_AGC040/D2.cpp
+++ 2019/1103_AGC040/D2.cpp
@@ -240,7 +240,7 @@ int main()
     {
       r = rational<ll>(0);
     }
-    ll M{included ? ok + 1 : ok};
+    ll M{included ? ok : ok};
     rational<ll> tmp_ans = (N - M - r) / N;
     ch_max(ans, tmp_ans);
   }
