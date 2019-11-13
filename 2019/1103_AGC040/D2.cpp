cd9da049b5752387fd6db6d62f974db341d0e479
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-11-13 19:56:29 +0900

    tried D2.cpp to 'D'

--- 2019/1103_AGC040/D2.cpp
+++ 2019/1103_AGC040/D2.cpp
@@ -210,7 +210,7 @@ int main()
   for (auto k = 0; k < N; k++)
   {
     ll B_k{get<1>(V[k])};
-    ll ok{N}, ng{-1};
+    ll ok{-1}, ng{N};
     ll tmp_sum{0};
     while (abs(ok - ng) > 1)
     {
@@ -220,7 +220,7 @@ int main()
       {
         tmp -= C_sum[t];
       }
-      if (tmp + B_k >= A_sum)
+      if (tmp + B_k < A_sum)
       {
         ok = t;
         tmp_sum = tmp;
