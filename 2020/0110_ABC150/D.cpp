7d68c04db56ca93c98b9442d9c2923b2cea5a457
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-01-10 23:24:36 +0900

    submit D.cpp to 'D - Semi Common Multiple' (abc150) [C++14 (GCC 5.4.1)]

--- 2020/0110_ABC150/D.cpp
+++ 2020/0110_ABC150/D.cpp
@@ -206,11 +206,11 @@ int main()
   for (auto i = 0; i < N; i++)
   {
     cin >> a[i];
-    if (a[i] % 2 == 1)
-    {
-      No();
-    }
     a[i] /= 2;
+    if (a[i] == 0)
+    {
+      sleep(100);
+    }
   }
   vector<int> cnt(N, 0);
   for (auto i = 0; i < N; i++)
