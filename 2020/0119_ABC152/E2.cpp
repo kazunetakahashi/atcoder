5075d2812ee1bbbbdbe5c3e78cc1fbf1ab820c6c
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-01-20 00:45:55 +0900

    submit E2.cpp to 'E - Flatten' (abc152) [C++14 (GCC 5.4.1)]

---
+++ 2020/0119_ABC152/E2.cpp
@@ -0,0 +1,327 @@
+#define DEBUG 1
+/**
+ * File    : E2.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2020/1/20 0:19:15
+ * Powered by Visual Studio Code
+ */
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+// ----- boost -----
+#include <boost/rational.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+// ----- using directives and manipulations -----
+using namespace std;
+using boost::rational;
+using boost::multiprecision::cpp_int;
+using ll = long long;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+// ----- constexpr for Mint and Combination -----
+constexpr ll MOD{1000000007LL};
+// constexpr ll MOD{998244353LL}; // be careful
+constexpr ll MAX_SIZE{3000010LL};
+// constexpr ll MAX_SIZE{30000010LL}; // if 10^7 is needed
+// ----- ch_max and ch_min -----
+template <typename T>
+void ch_max(T &left, T right)
+{
+  if (left < right)
+  {
+    left = right;
+  }
+}
+template <typename T>
+void ch_min(T &left, T right)
+{
+  if (left > right)
+  {
+    left = right;
+  }
+}
+// ----- Mint -----
+template <ll MOD = MOD>
+class Mint
+{
+public:
+  ll x;
+  Mint() : x{0LL} {}
+  Mint(ll x) : x{(x % MOD + MOD) % MOD} {}
+  Mint operator-() const { return x ? MOD - x : 0; }
+  Mint &operator+=(const Mint &a)
+  {
+    if ((x += a.x) >= MOD)
+    {
+      x -= MOD;
+    }
+    return *this;
+  }
+  Mint &operator-=(const Mint &a) { return *this += -a; }
+  Mint &operator*=(const Mint &a)
+  {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  Mint &operator/=(const Mint &a)
+  {
+    Mint b{a};
+    return *this *= b.power(MOD - 2);
+  }
+  Mint operator+(const Mint &a) const { return Mint(*this) += a; }
+  Mint operator-(const Mint &a) const { return Mint(*this) -= a; }
+  Mint operator*(const Mint &a) const { return Mint(*this) *= a; }
+  Mint operator/(const Mint &a) const { return Mint(*this) /= a; }
+  bool operator<(const Mint &a) const { return x < a.x; }
+  bool operator<=(const Mint &a) const { return x <= a.x; }
+  bool operator>(const Mint &a) const { return x > a.x; }
+  bool operator>=(const Mint &a) const { return x >= a.x; }
+  bool operator==(const Mint &a) const { return x == a.x; }
+  bool operator!=(const Mint &a) const { return !(*this == a); }
+  const Mint power(ll N)
+  {
+    if (N == 0)
+    {
+      return 1;
+    }
+    else if (N % 2 == 1)
+    {
+      return *this * power(N - 1);
+    }
+    else
+    {
+      Mint half = power(N / 2);
+      return half * half;
+    }
+  }
+};
+template <ll MOD>
+Mint<MOD> operator+(ll lhs, const Mint<MOD> &rhs)
+{
+  return rhs + lhs;
+}
+template <ll MOD>
+Mint<MOD> operator-(ll lhs, const Mint<MOD> &rhs)
+{
+  return -rhs + lhs;
+}
+template <ll MOD>
+Mint<MOD> operator*(ll lhs, const Mint<MOD> &rhs)
+{
+  return rhs * lhs;
+}
+template <ll MOD>
+Mint<MOD> operator/(ll lhs, const Mint<MOD> &rhs)
+{
+  return Mint<MOD>{lhs} / rhs;
+}
+template <ll MOD>
+istream &operator>>(istream &stream, Mint<MOD> &a)
+{
+  return stream >> a.x;
+}
+template <ll MOD>
+ostream &operator<<(ostream &stream, const Mint<MOD> &a)
+{
+  return stream << a.x;
+}
+// ----- Combination -----
+template <ll MOD = MOD, ll MAX_SIZE = MAX_SIZE>
+class Combination
+{
+public:
+  vector<Mint<MOD>> inv, fact, factinv;
+  Combination() : inv(MAX_SIZE), fact(MAX_SIZE), factinv(MAX_SIZE)
+  {
+    inv[1] = 1;
+    for (auto i = 2LL; i < MAX_SIZE; i++)
+    {
+      inv[i] = (-inv[MOD % i]) * (MOD / i);
+    }
+    fact[0] = factinv[0] = 1;
+    for (auto i = 1LL; i < MAX_SIZE; i++)
+    {
+      fact[i] = Mint<MOD>(i) * fact[i - 1];
+      factinv[i] = inv[i] * factinv[i - 1];
+    }
+  }
+  Mint<MOD> operator()(int n, int k)
+  {
+    if (n >= 0 && k >= 0 && n - k >= 0)
+    {
+      return fact[n] * factinv[k] * factinv[n - k];
+    }
+    return 0;
+  }
+  Mint<MOD> catalan(int x, int y)
+  {
+    return (*this)(x + y, y) - (*this)(x + y, y - 1);
+  }
+};
+// ----- for C++14 -----
+using mint = Mint<MOD>;
+using combination = Combination<MOD, MAX_SIZE>;
+template <typename T>
+T gcd(T x, T y) { return y ? gcd(y, x % y) : x; }
+template <typename T>
+T lcm(T x, T y) { return x / gcd(x, y) * y; }
+// ----- frequently used constexpr -----
+// constexpr double epsilon{1e-10};
+// constexpr ll infty{1000000000000000LL};
+// constexpr int dx[4] = {1, 0, -1, 0};
+// constexpr int dy[4] = {0, 1, 0, -1};
+// ----- Yes() and No() -----
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+void No()
+{
+  cout << "No" << endl;
+  exit(0);
+}
+// ----- main() -----
+
+class Sieve
+{
+  static constexpr ll MAX_SIZE{1000010LL};
+  ll N;
+  vector<bool> isprime;
+  vector<ll> prime_nums;
+
+public:
+  Sieve(ll N = MAX_SIZE) : N{N}, isprime(N, true), prime_nums{}
+  {
+    isprime[0] = isprime[1] = false;
+    for (auto i = 2; i < N; i++)
+    {
+      if (isprime[i])
+      {
+        prime_nums.push_back(i);
+        for (auto j = 2 * i; j < N; j += i)
+        {
+          isprime[j] = false;
+        }
+      }
+    }
+  }
+
+  bool is_prime(ll x)
+  { // 2 \leq x \leq MAX_SIZE^2
+    if (x < N)
+    {
+      return isprime[x];
+    }
+    for (auto e : prime_nums)
+    {
+      if (x % e == 0)
+        return false;
+    }
+    return true;
+  }
+
+  vector<ll> const &primes() const
+  {
+    return prime_nums;
+  }
+
+  vector<ll> factor_list(ll x)
+  {
+    vector<ll> res;
+    auto it{prime_nums.begin()};
+    while (x != 1 && it != prime_nums.end())
+    {
+      if (x % *it == 0)
+      {
+        res.push_back(*it);
+        x /= *it;
+      }
+      else
+      {
+        ++it;
+      }
+    }
+    if (x != 1)
+    {
+      res.push_back(x);
+    }
+    return res;
+  }
+
+  vector<tuple<ll, ll>> factor(ll x)
+  {
+    auto factors{factor_list(x)};
+    vector<tuple<ll, ll>> res{make_tuple(factors[0], 0)};
+    for (auto x : factors)
+    {
+      if (x == get<0>(res.back()))
+      {
+        get<1>(res.back())++;
+      }
+      else
+      {
+        res.emplace_back(x, 1);
+      }
+    }
+    return res;
+  }
+};
+
+int main()
+{
+  Sieve sieve;
+  int N;
+  cin >> N;
+  vector<ll> A(N);
+  for (auto i = 0; i < N; ++i)
+  {
+    cin >> A[i];
+  }
+  map<ll, ll> M;
+  for (auto i = 0; i < N; ++i)
+  {
+    auto f{sieve.factor(A[i])};
+    for (auto p : f)
+    {
+      ch_max(M[get<0>(p)], get<1>(p));
+    }
+  }
+  mint lcm{1};
+  for (auto p : M)
+  {
+    for (auto i = 0; i < get<1>(p); ++i)
+    {
+      lcm *= get<0>(p);
+    }
+  }
+  mint ans{0};
+  for (auto i = 0; i < N; ++i)
+  {
+    mint B{lcm / A[i]};
+    ans += B;
+  }
+  cout << ans << endl;
+}
