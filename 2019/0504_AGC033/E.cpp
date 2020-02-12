e8434ab65d18b7693afdf2f3fcf8f9478a89b673
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-02-12 18:23:57 +0900

    tried E.cpp to 'E'

---
+++ 2019/0504_AGC033/E.cpp
@@ -0,0 +1,355 @@
+#define DEBUG 1
+/**
+ * File    : E.cpp
+ * Author  : Kazune Takahashi
+ * Created : 2/12/2020, 4:45:12 PM
+ * Powered by Visual Studio Code
+ */
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+// ----- boost -----
+#include <boost/rational.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+// ----- using directives and manipulations -----
+using namespace std;
+using boost::rational;
+using boost::multiprecision::cpp_int;
+using ll = long long;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+// ----- constexpr for Mint and Combination -----
+constexpr ll MOD{1000000007LL};
+// constexpr ll MOD{998244353LL}; // be careful
+constexpr ll MAX_SIZE{3000010LL};
+// constexpr ll MAX_SIZE{30000010LL}; // if 10^7 is needed
+// ----- ch_max and ch_min -----
+template <typename T>
+void ch_max(T &left, T right)
+{
+  if (left < right)
+  {
+    left = right;
+  }
+}
+template <typename T>
+void ch_min(T &left, T right)
+{
+  if (left > right)
+  {
+    left = right;
+  }
+}
+// ----- Mint -----
+template <ll MOD = MOD>
+class Mint
+{
+public:
+  ll x;
+  Mint() : x{0LL} {}
+  Mint(ll x) : x{(x % MOD + MOD) % MOD} {}
+  Mint operator-() const { return x ? MOD - x : 0; }
+  Mint &operator+=(const Mint &a)
+  {
+    if ((x += a.x) >= MOD)
+    {
+      x -= MOD;
+    }
+    return *this;
+  }
+  Mint &operator-=(const Mint &a) { return *this += -a; }
+  Mint &operator++() { return *this += 1; }
+  Mint &operator++(int)
+  {
+    Mint tmp{*this};
+    ++*this;
+    return tmp;
+  }
+  Mint &operator--() { return *this -= 1; }
+  Mint &operator--(int)
+  {
+    Mint tmp{*this};
+    --*this;
+    return tmp;
+  }
+  Mint &operator*=(const Mint &a)
+  {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  Mint &operator/=(const Mint &a)
+  {
+    Mint b{a};
+    return *this *= b.power(MOD - 2);
+  }
+  Mint operator+(const Mint &a) const { return Mint(*this) += a; }
+  Mint operator-(const Mint &a) const { return Mint(*this) -= a; }
+  Mint operator*(const Mint &a) const { return Mint(*this) *= a; }
+  Mint operator/(const Mint &a) const { return Mint(*this) /= a; }
+  bool operator<(const Mint &a) const { return x < a.x; }
+  bool operator<=(const Mint &a) const { return x <= a.x; }
+  bool operator>(const Mint &a) const { return x > a.x; }
+  bool operator>=(const Mint &a) const { return x >= a.x; }
+  bool operator==(const Mint &a) const { return x == a.x; }
+  bool operator!=(const Mint &a) const { return !(*this == a); }
+  const Mint power(ll N)
+  {
+    if (N == 0)
+    {
+      return 1;
+    }
+    else if (N % 2 == 1)
+    {
+      return *this * power(N - 1);
+    }
+    else
+    {
+      Mint half = power(N / 2);
+      return half * half;
+    }
+  }
+};
+template <ll MOD>
+Mint<MOD> operator+(ll lhs, const Mint<MOD> &rhs)
+{
+  return rhs + lhs;
+}
+template <ll MOD>
+Mint<MOD> operator-(ll lhs, const Mint<MOD> &rhs)
+{
+  return -rhs + lhs;
+}
+template <ll MOD>
+Mint<MOD> operator*(ll lhs, const Mint<MOD> &rhs)
+{
+  return rhs * lhs;
+}
+template <ll MOD>
+Mint<MOD> operator/(ll lhs, const Mint<MOD> &rhs)
+{
+  return Mint<MOD>{lhs} / rhs;
+}
+template <ll MOD>
+istream &operator>>(istream &stream, Mint<MOD> &a)
+{
+  return stream >> a.x;
+}
+template <ll MOD>
+ostream &operator<<(ostream &stream, const Mint<MOD> &a)
+{
+  return stream << a.x;
+}
+// ----- Combination -----
+template <ll MOD = MOD, ll MAX_SIZE = MAX_SIZE>
+class Combination
+{
+public:
+  vector<Mint<MOD>> inv, fact, factinv;
+  Combination() : inv(MAX_SIZE), fact(MAX_SIZE), factinv(MAX_SIZE)
+  {
+    inv[1] = 1;
+    for (auto i = 2LL; i < MAX_SIZE; i++)
+    {
+      inv[i] = (-inv[MOD % i]) * (MOD / i);
+    }
+    fact[0] = factinv[0] = 1;
+    for (auto i = 1LL; i < MAX_SIZE; i++)
+    {
+      fact[i] = Mint<MOD>(i) * fact[i - 1];
+      factinv[i] = inv[i] * factinv[i - 1];
+    }
+  }
+  Mint<MOD> operator()(int n, int k)
+  {
+    if (n >= 0 && k >= 0 && n - k >= 0)
+    {
+      return fact[n] * factinv[k] * factinv[n - k];
+    }
+    return 0;
+  }
+  Mint<MOD> catalan(int x, int y)
+  {
+    return (*this)(x + y, y) - (*this)(x + y, y - 1);
+  }
+};
+// ----- for C++14 -----
+using mint = Mint<MOD>;
+using combination = Combination<MOD, MAX_SIZE>;
+template <typename T>
+T gcd(T x, T y) { return y ? gcd(y, x % y) : x; }
+template <typename T>
+T lcm(T x, T y) { return x / gcd(x, y) * y; }
+// ----- for C++17 -----
+template <typename T>
+int popcount(T x) // C++20
+{
+  int ans{0};
+  while (x != 0)
+  {
+    ans += x & 1;
+    x >>= 1;
+  }
+  return ans;
+}
+// ----- frequently used constexpr -----
+// constexpr double epsilon{1e-10};
+// constexpr ll infty{1000000000000000LL}; // or
+// constexpr int infty{1'000'000'010};
+// constexpr int dx[4] = {1, 0, -1, 0};
+// constexpr int dy[4] = {0, 1, 0, -1};
+// ----- Yes() and No() -----
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+void No()
+{
+  cout << "No" << endl;
+  exit(0);
+}
+// ----- main() -----
+
+class Solve
+{
+  bool all_red;
+  int N, M;
+  string S;
+  int X;
+
+public:
+  Solve(int N, int M, string S) : all_red{false}, N{N}, M{M}, S{S}, X{max_length()} {}
+
+  void flush()
+  {
+    if (all_red)
+    {
+      cout << solve_all_red() << endl;
+    }
+    else
+    {
+      cout << solve_normal() << endl;
+    }
+  };
+
+private:
+  mint solve_all_red()
+  {
+    vector<vector<mint>> dp(N + 1, vector<mint>(2, 0));
+    dp[0][0] = 1;
+    for (auto i = 1; i <= N; ++i)
+    {
+      dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+      dp[i][1] = dp[i - 1][0];
+    }
+    return 3 * dp[N - 2][0] + dp[N - 2][1];
+  }
+
+  mint solve_normal()
+  {
+    if (N % 2 == 1)
+    {
+      return 0;
+    }
+    int N{Solve::N / 2}, X{Solve::X / 2};
+    vector<mint> DP(N + 1, 0);
+    DP[0] = 1;
+    for (auto i = 1; i <= N; ++i)
+    {
+      DP[i] = 2 * DP[i - 1];
+      if (i - 2 - X >= 0)
+      {
+        DP[i] -= DP[i - 2 - X];
+      }
+    }
+    mint ans{0};
+    for (auto K = 0; K <= X; ++K)
+    {
+      if (N - K - 2 >= 0)
+      {
+        ans += (K + 1) * DP[N - K - 2];
+      }
+    }
+    return ans;
+  }
+
+  int max_length()
+  {
+    if (S[0] == 'B')
+    {
+      for (auto &x : S)
+      {
+        if (x == 'R')
+        {
+          x = 'B';
+        }
+        else
+        {
+          x = 'R';
+        }
+      }
+    }
+    vector<int> V;
+    int now{0};
+    for (auto i = 0; i < M; ++i)
+    {
+      if (S[i] == 'R')
+      {
+        ++now;
+      }
+      else if (now > 0)
+      {
+        V.push_back(now);
+        now = 0;
+      }
+    }
+    if (V.empty())
+    {
+      all_red = true;
+      return N;
+    }
+    if (V[0] % 2 == 0)
+    {
+      ++V[0];
+    }
+    for (auto it = V.begin() + 1; it != V.end(); ++it)
+    {
+      if (*it % 2 == 0)
+      {
+        *it = N;
+      }
+    }
+    return *min_element(V.begin(), V.end());
+  }
+};
+
+int main()
+{
+  int N, M;
+  string S;
+  cin >> N >> M >> S;
+  Solve solve(N, M, S);
+  solve.flush();
+}
