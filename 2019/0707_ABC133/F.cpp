2171e53ac2fc08662c8ef87cd56229719fb7178d
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-05-19 17:26:50 +0900

    submit F.cpp to 'F - Colorful Tree' (abc133) [C++14 (GCC 5.4.1)]

--- 2019/0707_ABC133/F.cpp
+++ 2019/0707_ABC133/F.cpp
@@ -233,116 +233,137 @@ void No()
 
 // ----- LCA -----
 
-template <typename T = ll>
 class LCA
 {
+  // helper classes
+  struct Edge
+  {
+    // Initialized by initializer list. Take care fore the order of the field.
+    int src, dst;
+    ll cost;
+  };
+
+  struct Vertex
+  {
+    int depth;
+    ll length;
+  };
+
+  // fields
   int N, root, L;
-  vector<vector<int>> to;
-  vector<vector<T>> co;
-  vector<int> dep;
-  vector<T> costs;
-  vector<vector<int>> par;
+  vector<vector<Edge>> E;
+  vector<Vertex> V;
+  vector<vector<int>> ancestors;
 
+  // methods
 public:
-  LCA(int n) : N{n}, L{0}, to(n), co(n), dep(n), costs(n)
-  {
-    while ((1 << L) < N)
-    {
-      ++L;
-    }
-    par = vector<vector<int>>(N + 1, vector<int>(L, N));
-  }
-
-  void add_edge(int a, int b, T c = 0)
-  {
-    to[a].push_back(b);
-    co[a].push_back(c);
-    to[b].push_back(a);
-    co[b].push_back(c);
-  }
-
-  void init(int _root)
-  {
-    root = _root;
-    dfs(root);
-    for (auto i = 0; i < L - 1; i++)
-    {
-      for (auto v = 0; v < N; v++)
-      {
-        if (par[v][i] != -1)
-        {
-          par[v][i + 1] = par[par[v][i]][i];
-        }
-      }
-    }
-  }
+  LCA(int N, int root = 0);
+  void add_edge(int a, int b, ll c = 0);
+  void init();
+  void init(int root); // after adding all edges.
 
   // LCA
-  int operator()(int a, int b)
-  {
-    if (dep[a] > dep[b])
-    {
-      swap(a, b);
-    }
-    int gap = dep[b] - dep[a];
-    for (auto i = L - 1; i >= 0; i--)
-    {
-      int len = 1 << i;
-      if (gap >= len)
-      {
-        gap -= len;
-        b = par[b][i];
-      }
-    }
-    if (a == b)
-    {
-      return a;
-    }
-    for (auto i = L - 1; i >= 0; i--)
-    {
-      int na = par[a][i];
-      int nb = par[b][i];
-      if (na != nb)
-      {
-        a = na;
-        b = nb;
-      }
-    }
-    return par[a][0];
-  }
+  int operator()(int a, int b);
 
-  int depth(int a, int b)
-  {
-    int c = (*this)(a, b);
-    return dep[a] + dep[b] - 2 * dep[c];
-  }
-
-  T cost(int a, int b)
-  {
-    int c = (*this)(a, b);
-    return costs[a] + costs[b] - 2 * costs[c];
-  }
+  int depth(int a, int b) { return V[a].depth + V[b].depth - 2 * V[(*this)(a, b)].depth; }
+  int depth(int v) { return depth(v, root); }
+  ll length(int a, int b) { return V[a].length + V[b].length - 2 * V[(*this)(a, b)].length; }
+  ll length(int v) { return length(v, root); }
+  int parent(int v) { return ancestors[v][0]; }
 
 private:
-  void dfs(int v, int d = 0, T c = 0, int p = -1)
+  void dfs(int v, int d = 0, ll l = 0, int p = -1);
+};
+
+// LCA: implement
+
+LCA::LCA(int N, int root) : N{N}, root{root}, L{0}, E(N), V(N)
+{
+  while ((1 << L) < N)
   {
-    if (p != -1)
+    ++L;
+  }
+  ancestors = vector<vector<int>>(N + 1, vector<int>(L, N));
+}
+
+void LCA::add_edge(int a, int b, ll c)
+{
+  E[a].push_back(LCA::Edge{a, b, c});
+  E[b].push_back(LCA::Edge{b, a, c});
+}
+
+void LCA::init(int root)
+{
+  LCA::root = root;
+  init();
+}
+
+void LCA::init()
+{
+  dfs(root);
+  for (auto i = 0; i < L - 1; i++)
+  {
+    for (auto v = 0; v < N; v++)
     {
-      par[v][0] = p;
-    }
-    dep[v] = d;
-    costs[v] = c;
-    for (auto i = 0u; i < to[v].size(); i++)
-    {
-      int u = to[v][i];
-      if (u == p)
+      if (ancestors[v][i] != -1)
       {
-        continue;
+        ancestors[v][i + 1] = ancestors[ancestors[v][i]][i];
       }
-      dfs(u, d + 1, c + co[v][i], v);
     }
   }
-};
+}
+
+int LCA::operator()(int a, int b)
+{
+  if (V[a].depth > V[b].depth)
+  {
+    swap(a, b);
+  }
+  int gap = V[b].depth - V[a].depth;
+  for (auto i = L - 1; i >= 0; i--)
+  {
+    int len{1 << i};
+    if (gap >= len)
+    {
+      gap -= len;
+      b = ancestors[b][i];
+    }
+  }
+  if (a == b)
+  {
+    return a;
+  }
+  for (auto i = L - 1; i >= 0; i--)
+  {
+    int na{ancestors[a][i]};
+    int nb{ancestors[b][i]};
+    if (na != nb)
+    {
+      a = na;
+      b = nb;
+    }
+  }
+  return ancestors[a][0];
+}
+
+void LCA::dfs(int v, int d, ll l, int p)
+{
+  if (p != -1)
+  {
+    ancestors[v][0] = p;
+  }
+  V[v].depth = d;
+  V[v].length = l;
+  for (auto const &e : E[v])
+  {
+    int u{e.dst};
+    if (u == p)
+    {
+      continue;
+    }
+    dfs(u, d + 1, l + e.cost, v);
+  }
+}
 
 // ----- main() -----
 
@@ -361,7 +382,7 @@ class Solve
   int N, Q;
   vector<vector<Edge>> V;
   vector<vector<Query>> W;
-  LCA<int> lca;
+  LCA lca;
   vector<int> ans;
   vector<int> sum, cnt;
 
@@ -390,7 +411,7 @@ public:
       W[u].push_back({i, x, y, 1});
       W[v].push_back({i, x, y, 1});
       W[lca(u, v)].push_back({i, x, y, -2});
-      ans[i] = lca.cost(u, v);
+      ans[i] = lca.length(u, v);
     }
   }
 
