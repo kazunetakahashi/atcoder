e3b8479ac5fdfadb4007c2388d36835d876f31e6
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-06-10 01:19:24 +0900

    tried C.cpp to 'C'

--- 2020/0202_ABC147/C.cpp
+++ 2020/0202_ABC147/C.cpp
@@ -252,8 +252,11 @@ int main()
       }
     }
   }
+#if DEBUG == 1
+  cerr << "N = " << N << endl;
+#endif
   int ans{0};
-  for (auto i{0}; i < (1 << N); ++i)
+  for (auto i{0}; i < 1 << N; ++i)
   {
 #if DEBUG == 1
     cerr << "i = " << i << endl;
