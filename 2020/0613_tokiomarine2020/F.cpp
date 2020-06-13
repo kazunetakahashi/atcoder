60000ff6cf8c97632604b6e570969c751d296b3e
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-06-14 05:56:49 +0900

    tried F.cpp to 'F'

--- 2020/0613_tokiomarine2020/F.cpp
+++ 2020/0613_tokiomarine2020/F.cpp
@@ -235,7 +235,7 @@ public:
     ll ans{0};
     for (auto s{0LL}; s < W - 1; ++s)
     {
-      for (auto y{0LL}; rhs_bound(s, y) >= 0 && y < H - 1; ++y)
+      for (auto y{1LL}; rhs_bound(s, y) >= 0 && y < H - 1; ++y)
       {
         ll tmp{calc(s, y)};
         if (s == 0)
