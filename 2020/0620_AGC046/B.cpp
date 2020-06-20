df1d96654584e2fdfdedde50e1fade1f23210662
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-06-20 21:31:20 +0900

    tried B.cpp to 'B'

--- 2020/0620_AGC046/B.cpp
+++ 2020/0620_AGC046/B.cpp
@@ -258,6 +258,9 @@ int main()
         continue;
       }
       dp[i][j] = dp[i - 1][j] * j + dp[i][j - 1] * i - dp[i - 1][j - 1] * (1 + (i - 1) * (j - 1));
+      #if DEBUG == 1
+      cerr << "dp[" << i << "][" << j << "] = " << dp[i][j] << endl;
+      #endif
     }
   }
   cout << dp[C][D] << endl;
