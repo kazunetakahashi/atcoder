a9f530814b121d4fb2e15306b2882f20f219d4a0
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-06-22 02:58:56 +0900

    tried F.cpp to 'F'

---
+++ 2020/0622_ABC161/F.cpp
@@ -0,0 +1,316 @@
+#define DEBUG 1
+/**
+ * File    : F.cpp
+ * Author  : Kazune Takahashi
+ * Created : 6/22/2020, 1:47:53 AM
+ * Powered by Visual Studio Code
+ */
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+// ----- boost -----
+#include <boost/integer/common_factor_rt.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/rational.hpp>
+// ----- using directives and manipulations -----
+using namespace std;
+using boost::rational;
+using boost::integer::gcd; // for C++14 or for cpp_int
+using boost::integer::lcm; // for C++14 or for cpp_int
+using boost::multiprecision::cpp_int;
+using ll = long long;
+using ld = long double;
+template <typename T>
+using max_heap = priority_queue<T>;
+template <typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+// ----- constexpr for Mint and Combination -----
+constexpr ll MOD{1'000'000'007LL};
+// constexpr ll MOD{998'244'353LL}; // be careful
+constexpr ll MAX_SIZE{3'000'010LL};
+// constexpr ll MAX_SIZE{30'000'010LL}; // if 10^7 is needed
+// ----- ch_max and ch_min -----
+template <typename T>
+bool ch_max(T &left, T right)
+{
+  if (left < right)
+  {
+    left = right;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool ch_min(T &left, T right)
+{
+  if (left > right)
+  {
+    left = right;
+    return true;
+  }
+  return false;
+}
+// ----- Mint -----
+template <ll MOD = MOD>
+class Mint
+{
+public:
+  ll x;
+  Mint() : x{0LL} {}
+  Mint(ll x) : x{(x % MOD + MOD) % MOD} {}
+  Mint operator-() const { return x ? MOD - x : 0; }
+  Mint &operator+=(Mint const &a)
+  {
+    if ((x += a.x) >= MOD)
+    {
+      x -= MOD;
+    }
+    return *this;
+  }
+  Mint &operator-=(Mint const &a) { return *this += -a; }
+  Mint &operator++() { return *this += 1; }
+  Mint operator++(int)
+  {
+    Mint tmp{*this};
+    ++*this;
+    return tmp;
+  }
+  Mint &operator--() { return *this -= 1; }
+  Mint operator--(int)
+  {
+    Mint tmp{*this};
+    --*this;
+    return tmp;
+  }
+  Mint &operator*=(Mint const &a)
+  {
+    (x *= a.x) %= MOD;
+    return *this;
+  }
+  Mint &operator/=(Mint const &a)
+  {
+    Mint b{a};
+    return *this *= b.power(MOD - 2);
+  }
+  Mint operator+(Mint const &a) const { return Mint(*this) += a; }
+  Mint operator-(Mint const &a) const { return Mint(*this) -= a; }
+  Mint operator*(Mint const &a) const { return Mint(*this) *= a; }
+  Mint operator/(Mint const &a) const { return Mint(*this) /= a; }
+  bool operator<(Mint const &a) const { return x < a.x; }
+  bool operator<=(Mint const &a) const { return x <= a.x; }
+  bool operator>(Mint const &a) const { return x > a.x; }
+  bool operator>=(Mint const &a) const { return x >= a.x; }
+  bool operator==(Mint const &a) const { return x == a.x; }
+  bool operator!=(Mint const &a) const { return !(*this == a); }
+  Mint power(ll N) const
+  {
+    if (N == 0)
+    {
+      return 1;
+    }
+    else if (N % 2 == 1)
+    {
+      return *this * power(N - 1);
+    }
+    else
+    {
+      Mint half = power(N / 2);
+      return half * half;
+    }
+  }
+};
+template <ll MOD>
+Mint<MOD> operator+(ll lhs, Mint<MOD> const &rhs) { return rhs + lhs; }
+template <ll MOD>
+Mint<MOD> operator-(ll lhs, Mint<MOD> const &rhs) { return -rhs + lhs; }
+template <ll MOD>
+Mint<MOD> operator*(ll lhs, Mint<MOD> const &rhs) { return rhs * lhs; }
+template <ll MOD>
+Mint<MOD> operator/(ll lhs, Mint<MOD> const &rhs) { return Mint<MOD>{lhs} / rhs; }
+template <ll MOD>
+istream &operator>>(istream &stream, Mint<MOD> &a) { return stream >> a.x; }
+template <ll MOD>
+ostream &operator<<(ostream &stream, Mint<MOD> const &a) { return stream << a.x; }
+// ----- Combination -----
+template <ll MOD = MOD, ll MAX_SIZE = MAX_SIZE>
+class Combination
+{
+public:
+  vector<Mint<MOD>> inv, fact, factinv;
+  Combination() : inv(MAX_SIZE), fact(MAX_SIZE), factinv(MAX_SIZE)
+  {
+    inv[1] = 1;
+    for (auto i{2LL}; i < MAX_SIZE; i++)
+    {
+      inv[i] = (-inv[MOD % i]) * (MOD / i);
+    }
+    fact[0] = factinv[0] = 1;
+    for (auto i{1LL}; i < MAX_SIZE; i++)
+    {
+      fact[i] = Mint<MOD>(i) * fact[i - 1];
+      factinv[i] = inv[i] * factinv[i - 1];
+    }
+  }
+  Mint<MOD> operator()(int n, int k)
+  {
+    if (n >= 0 && k >= 0 && n - k >= 0)
+    {
+      return fact[n] * factinv[k] * factinv[n - k];
+    }
+    return 0;
+  }
+  Mint<MOD> catalan(int x, int y)
+  {
+    return (*this)(x + y, y) - (*this)(x + y, y - 1);
+  }
+};
+// ----- for C++14 -----
+using mint = Mint<MOD>;
+using combination = Combination<MOD, MAX_SIZE>;
+// ----- for C++17 -----
+template <typename T, typename enable_if<is_integral<T>::value>::type * = nullptr>
+size_t popcount(T x) { return bitset<64>(x).count(); }
+size_t popcount(string const &S) { return bitset<200010>{S}.count(); }
+// ----- Infty -----
+template <typename T>
+constexpr T Infty() { return numeric_limits<T>::max(); }
+template <typename T>
+constexpr T mInfty() { return numeric_limits<T>::min(); }
+// ----- frequently used constexpr -----
+// constexpr double epsilon{1e-10};
+// constexpr ll infty{1'000'000'000'000'010LL}; // or
+// constexpr int infty{1'000'000'010};
+// constexpr int dx[4] = {1, 0, -1, 0};
+// constexpr int dy[4] = {0, 1, 0, -1};
+// ----- Yes() and No() -----
+void Yes()
+{
+  cout << "Yes" << endl;
+  exit(0);
+}
+void No()
+{
+  cout << "No" << endl;
+  exit(0);
+}
+
+// ----- Solve -----
+
+constexpr ll M{1'000'010};
+
+class Solve
+{
+  ll N;
+
+public:
+  Solve(ll N) : N{N}
+  {
+    for (auto K{2}; K <= N; ++K)
+    {
+      g(N, K);
+    }
+  }
+
+  void flush()
+  {
+    ll ans{0};
+    for (auto K{2LL}; K <= min(N, M); ++K)
+    {
+      if (g(N, K) == 1)
+      {
+        ++ans;
+      }
+    }
+    ll X{N - 1};
+    for (auto i{1LL}; i <= M; ++i)
+    {
+      if (X % i == 0)
+      {
+        auto y{X / i};
+        if (y <= N)
+        {
+          ++ans;
+        }
+      }
+    }
+    cout << ans << endl;
+  }
+
+private:
+  ll g(ll N, ll K)
+  {
+    while (N % K == 0)
+    {
+      N /= K;
+    }
+    return N % K;
+  }
+
+  ll g_info(ll N, ll K)
+  {
+#if DEBUG == 1
+    cerr << "K = " << K << " : " << N << " ";
+#endif
+    while (N >= K)
+    {
+      N = f_info(N, K);
+#if DEBUG == 1
+      cerr << N << " ";
+#endif
+    }
+#if DEBUG == 1
+    cerr << endl;
+#endif
+    return N;
+  }
+
+  ll f_info(ll N, ll K)
+  {
+    if (N % K == 0)
+    {
+#if DEBUG == 1
+      cerr << "(div) ";
+#endif
+      return N / K;
+    }
+    else
+    {
+#if DEBUG == 1
+      cerr << "(sub) ";
+#endif
+      return N - K;
+    }
+  }
+};
+
+// ----- main() -----
+
+int main()
+{
+  ll N;
+  cin >> N;
+  Solve solve(N);
+  solve.flush();
+}
