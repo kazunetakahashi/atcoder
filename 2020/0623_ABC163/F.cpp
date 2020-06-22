6104397f71f4833ee9176c0c53003fa640518be0
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-06-23 08:48:32 +0900

    tried F.cpp to 'F'

--- 2020/0623_ABC163/F.cpp
+++ 2020/0623_ABC163/F.cpp
@@ -368,6 +368,9 @@ public:
 private:
   ll dfs(ll src = 0, ll parent = -1)
   {
+#if DEBUG == 1
+    cout << "dfs(" << src << ")" << endl;
+#endif
     auto src_id{id++};
     auto &subtree_queue{Q[C[src]]};
     for (auto const &e : V[src])
