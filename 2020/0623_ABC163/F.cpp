cbb64d98cf5fd75400bcde32f40068cd5cacd498
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-06-23 10:39:22 +0900

    tried F.cpp to 'F'

--- 2020/0623_ABC163/F.cpp
+++ 2020/0623_ABC163/F.cpp
@@ -368,7 +368,7 @@ private:
   ll dfs(ll src = 0, ll parent = -1)
   {
 #if DEBUG == 1
-    cerr << "src = " << src << endl;
+    cout << "src = " << src << endl;
 #endif
     auto src_id{id++};
     auto &subtree_queue{Q[C[src]]};
