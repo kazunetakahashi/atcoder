dda24d57bc7bdb2bed2a4dbcee32fb3b886bf958
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-07-04 01:38:00 +0900

    submit D.cpp to 'D - アンバランス' (arc059) [C++ (GCC 9.2.1)]

--- 2020/0704_ARC059/D.cpp
+++ 2020/0704_ARC059/D.cpp
@@ -259,7 +259,7 @@ int main()
   int n = s.size();
   for (auto k{2}; k <= 3; ++k)
   {
-    for (auto i{0}; i + k - 1 < n; ++i)
+    for (auto i{0}; i + k < n; ++i)
     {
       if (unbalanced(s.substr(i, k)))
       {
