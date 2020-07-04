349c2a9f7087ca4e53470abfa4804c21d4ac5bfe
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-07-04 18:30:47 +0900

    tried E.cpp to 'E'

--- 2019/0525_ABC127/past2/E.cpp
+++ 2019/0525_ABC127/past2/E.cpp
@@ -2,7 +2,7 @@
 /**
  * File    : E.cpp
  * Author  : Kazune Takahashi
- * Created : 7/4/2020, 6:11:18 PM
+ * Created : 5/16/2020, 9:43:39 PM
  * Powered by Visual Studio Code
  */
 #include <algorithm>
@@ -10,7 +10,6 @@
 #include <cassert>
 #include <cctype>
 #include <chrono>
-#include <climits>
 #include <cmath>
 #include <complex>
 #include <cstdint>
@@ -19,7 +18,6 @@
 #include <functional>
 #include <iomanip>
 #include <iostream>
-#include <limits>
 #include <map>
 #include <queue>
 #include <random>
@@ -31,46 +29,38 @@
 #include <unordered_set>
 #include <vector>
 // ----- boost -----
-#include <boost/integer/common_factor_rt.hpp>
-#include <boost/multiprecision/cpp_int.hpp>
 #include <boost/rational.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
 // ----- using directives and manipulations -----
 using namespace std;
 using boost::rational;
-using boost::integer::gcd; // for C++14 or for cpp_int
-using boost::integer::lcm; // for C++14 or for cpp_int
 using boost::multiprecision::cpp_int;
 using ll = long long;
-using ld = long double;
 template <typename T>
 using max_heap = priority_queue<T>;
 template <typename T>
 using min_heap = priority_queue<T, vector<T>, greater<T>>;
 // ----- constexpr for Mint and Combination -----
-constexpr ll MOD{1'000'000'007LL};
-// constexpr ll MOD{998'244'353LL}; // be careful
-constexpr ll MAX_SIZE{3'000'010LL};
-// constexpr ll MAX_SIZE{30'000'010LL}; // if 10^7 is needed
+constexpr ll MOD{1000000007LL};
+// constexpr ll MOD{998244353LL}; // be careful
+constexpr ll MAX_SIZE{3000010LL};
+// constexpr ll MAX_SIZE{30000010LL}; // if 10^7 is needed
 // ----- ch_max and ch_min -----
 template <typename T>
-bool ch_max(T &left, T right)
+void ch_max(T &left, T right)
 {
   if (left < right)
   {
     left = right;
-    return true;
   }
-  return false;
 }
 template <typename T>
-bool ch_min(T &left, T right)
+void ch_min(T &left, T right)
 {
   if (left > right)
   {
     left = right;
-    return true;
   }
-  return false;
 }
 // ----- Mint -----
 template <ll MOD = MOD>
@@ -81,7 +71,7 @@ public:
   Mint() : x{0LL} {}
   Mint(ll x) : x{(x % MOD + MOD) % MOD} {}
   Mint operator-() const { return x ? MOD - x : 0; }
-  Mint &operator+=(Mint const &a)
+  Mint &operator+=(const Mint &a)
   {
     if ((x += a.x) >= MOD)
     {
@@ -89,42 +79,42 @@ public:
     }
     return *this;
   }
-  Mint &operator-=(Mint const &a) { return *this += -a; }
+  Mint &operator-=(const Mint &a) { return *this += -a; }
   Mint &operator++() { return *this += 1; }
-  Mint operator++(int)
+  Mint &operator++(int)
   {
     Mint tmp{*this};
     ++*this;
     return tmp;
   }
   Mint &operator--() { return *this -= 1; }
-  Mint operator--(int)
+  Mint &operator--(int)
   {
     Mint tmp{*this};
     --*this;
     return tmp;
   }
-  Mint &operator*=(Mint const &a)
+  Mint &operator*=(const Mint &a)
   {
     (x *= a.x) %= MOD;
     return *this;
   }
-  Mint &operator/=(Mint const &a)
+  Mint &operator/=(const Mint &a)
   {
     Mint b{a};
     return *this *= b.power(MOD - 2);
   }
-  Mint operator+(Mint const &a) const { return Mint(*this) += a; }
-  Mint operator-(Mint const &a) const { return Mint(*this) -= a; }
-  Mint operator*(Mint const &a) const { return Mint(*this) *= a; }
-  Mint operator/(Mint const &a) const { return Mint(*this) /= a; }
-  bool operator<(Mint const &a) const { return x < a.x; }
-  bool operator<=(Mint const &a) const { return x <= a.x; }
-  bool operator>(Mint const &a) const { return x > a.x; }
-  bool operator>=(Mint const &a) const { return x >= a.x; }
-  bool operator==(Mint const &a) const { return x == a.x; }
-  bool operator!=(Mint const &a) const { return !(*this == a); }
-  Mint power(ll N) const
+  Mint operator+(const Mint &a) const { return Mint(*this) += a; }
+  Mint operator-(const Mint &a) const { return Mint(*this) -= a; }
+  Mint operator*(const Mint &a) const { return Mint(*this) *= a; }
+  Mint operator/(const Mint &a) const { return Mint(*this) /= a; }
+  bool operator<(const Mint &a) const { return x < a.x; }
+  bool operator<=(const Mint &a) const { return x <= a.x; }
+  bool operator>(const Mint &a) const { return x > a.x; }
+  bool operator>=(const Mint &a) const { return x >= a.x; }
+  bool operator==(const Mint &a) const { return x == a.x; }
+  bool operator!=(const Mint &a) const { return !(*this == a); }
+  const Mint power(ll N)
   {
     if (N == 0)
     {
@@ -142,17 +132,35 @@ public:
   }
 };
 template <ll MOD>
-Mint<MOD> operator+(ll lhs, Mint<MOD> const &rhs) { return rhs + lhs; }
+Mint<MOD> operator+(ll lhs, const Mint<MOD> &rhs)
+{
+  return rhs + lhs;
+}
 template <ll MOD>
-Mint<MOD> operator-(ll lhs, Mint<MOD> const &rhs) { return -rhs + lhs; }
+Mint<MOD> operator-(ll lhs, const Mint<MOD> &rhs)
+{
+  return -rhs + lhs;
+}
 template <ll MOD>
-Mint<MOD> operator*(ll lhs, Mint<MOD> const &rhs) { return rhs * lhs; }
+Mint<MOD> operator*(ll lhs, const Mint<MOD> &rhs)
+{
+  return rhs * lhs;
+}
 template <ll MOD>
-Mint<MOD> operator/(ll lhs, Mint<MOD> const &rhs) { return Mint<MOD>{lhs} / rhs; }
+Mint<MOD> operator/(ll lhs, const Mint<MOD> &rhs)
+{
+  return Mint<MOD>{lhs} / rhs;
+}
 template <ll MOD>
-istream &operator>>(istream &stream, Mint<MOD> &a) { return stream >> a.x; }
+istream &operator>>(istream &stream, Mint<MOD> &a)
+{
+  return stream >> a.x;
+}
 template <ll MOD>
-ostream &operator<<(ostream &stream, Mint<MOD> const &a) { return stream << a.x; }
+ostream &operator<<(ostream &stream, const Mint<MOD> &a)
+{
+  return stream << a.x;
+}
 // ----- Combination -----
 template <ll MOD = MOD, ll MAX_SIZE = MAX_SIZE>
 class Combination
@@ -162,12 +170,12 @@ public:
   Combination() : inv(MAX_SIZE), fact(MAX_SIZE), factinv(MAX_SIZE)
   {
     inv[1] = 1;
-    for (auto i{2LL}; i < MAX_SIZE; i++)
+    for (auto i = 2LL; i < MAX_SIZE; i++)
     {
       inv[i] = (-inv[MOD % i]) * (MOD / i);
     }
     fact[0] = factinv[0] = 1;
-    for (auto i{1LL}; i < MAX_SIZE; i++)
+    for (auto i = 1LL; i < MAX_SIZE; i++)
     {
       fact[i] = Mint<MOD>(i) * fact[i - 1];
       factinv[i] = inv[i] * factinv[i - 1];
@@ -189,18 +197,25 @@ public:
 // ----- for C++14 -----
 using mint = Mint<MOD>;
 using combination = Combination<MOD, MAX_SIZE>;
+template <typename T>
+T gcd(T x, T y) { return y ? gcd(y, x % y) : x; }
+template <typename T>
+T lcm(T x, T y) { return x / gcd(x, y) * y; }
 // ----- for C++17 -----
-template <typename T, typename enable_if<is_integral<T>::value>::type * = nullptr>
-size_t popcount(T x) { return bitset<64>(x).count(); }
-size_t popcount(string const &S) { return bitset<200010>{S}.count(); }
-// ----- Infty -----
 template <typename T>
-constexpr T Infty() { return numeric_limits<T>::max(); }
-template <typename T>
-constexpr T mInfty() { return numeric_limits<T>::min(); }
+int popcount(T x) // C++20
+{
+  int ans{0};
+  while (x != 0)
+  {
+    ans += x & 1;
+    x >>= 1;
+  }
+  return ans;
+}
 // ----- frequently used constexpr -----
 // constexpr double epsilon{1e-10};
-// constexpr ll infty{1'000'000'000'000'010LL}; // or
+// constexpr ll infty{1000000000000000LL}; // or
 // constexpr int infty{1'000'000'010};
 // constexpr int dx[4] = {1, 0, -1, 0};
 // constexpr int dy[4] = {0, 1, 0, -1};
@@ -215,41 +230,19 @@ void No()
   cout << "No" << endl;
   exit(0);
 }
-
-// ----- Solve -----
+// ----- main() -----
 
 combination C;
 
-class Solve
+mint f(ll N, ll M, ll K)
 {
-  ll n, m, k;
-
-public:
-  Solve(ll n, ll m, ll k) : n{n}, m{m}, k{k}
-  {
-  }
-
-  ll answer()
-  {
-    mint ans{0};
-    for (auto j{0LL}; j < n; ++j)
-    {
-      ans += j * (j + 1) / 2;
-    }
-    ans *= mint{n} * n * C(n * m - 2, k - 2);
-    cout << ans << endl;
-  }
-
-private:
-};
-
-// ----- main() -----
+  mint n{N}, m{M}, k{K};
+  return n * n * C.inv[6] * C(N * M - 2, K - 2) * (m - 1) * m * (m + 1);
+}
 
 int main()
 {
-  ll n, m, k;
-  cin >> n >> m >> k;
-  Solve solve(n, m, k);
-  Solve solve2(m, n, k);
-  cout << solve.answer() + solve2.answer() << endl;
+  ll N, M, K;
+  cin >> N >> M >> K;
+  cout << f(N, M, K) + f(M, N, K) << endl;
 }
