6fcf35c7fd55bf866bb6033224f1ab538e7a6a2b
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-07-05 22:40:13 +0900

    submit B.cpp to 'B - Judge Status Summary' (abc173) [C++ (GCC 9.2.1)]

--- 2020/0705_ABC173/B.cpp
+++ 2020/0705_ABC173/B.cpp
@@ -245,32 +245,17 @@ int main()
 
 int main()
 {
-  int ac{0}, wa{0}, tle{0}, re{0};
   int n;
   cin >> n;
+  map<string, ll> m;
   for (auto i{0}; i < n; ++i)
   {
     string s;
     cin >> s;
-    if (s == "AC")
-    {
-      ++ac;
-    }
-    if (s == "WA")
-    {
-      ++wa;
-    }
-    if (s == "TLE")
-    {
-      ++tle;
-    }
-    if (s == "RE")
-    {
-      ++re;
-    }
+    m[s]++;
   }
-  cout << "AC x " << ac << endl;
-  cout << "WA x " << wa << endl;
-  cout << "TLE x " << tle << endl;
-  cout << "RE x " << re << endl;
+  cout << "AC x " << m["AC"] << endl;
+  cout << "WA x " << m["WA"] << endl;
+  cout << "TLE x " << m["TLE"] << endl;
+  cout << "RE x " << m["RE"] << endl;
 }
