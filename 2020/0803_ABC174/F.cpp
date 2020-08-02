b0249c1ea02cd02efa3411fcf6d5db89c9069064
Kazune Takahashi <kazunetakahashi@gmail.com>
2020-08-03 07:37:43 +0900

    submit F.cpp to 'F - Range Set Query' (abc174) [C++ (GCC 9.2.1)]

--- 2020/0803_ABC174/F.cpp
+++ 2020/0803_ABC174/F.cpp
@@ -610,6 +610,54 @@ SegTree<Monoid, Monoid> RSQ_RUQ(int N)
   return RSQ_RUQ<Monoid>(N, 0);
 }
 
+// ----- BIT -----
+// referring to http://hos.ac/slides/20140319_bit.pdf
+
+template <typename T>
+class BIT
+{
+  int N;
+  vector<T> data;
+
+public:
+  BIT() {}
+  BIT(int N) : N{N}, data(N + 1) {}
+
+  void add(int i, T w) // data[i] += w;
+  {
+    for (; i < N; i |= i + 1)
+    {
+      data[i] += w;
+    }
+  }
+
+  void update(int i, T w) // data[i] = w;
+  {
+    add(i, w - (*this)[i]);
+  }
+
+  T sum(int l, int r) // data[l] + \dots + data[r - 1]
+  {
+    return sum(r) - sum(l);
+  }
+
+  T operator[](int i) // data[i]
+  {
+    return sum(i, i + 1);
+  }
+
+private:
+  T sum(int i) // data[0] + \dots + data[i - 1]
+  {
+    T res{0};
+    for (--i; i >= 0; i = (i & (i + 1)) - 1)
+    {
+      res += data[i];
+    }
+    return res;
+  }
+};
+
 // ----- main() -----
 
 /*
@@ -642,7 +690,7 @@ int main()
     v[r].push_back(Info{i, l});
   }
   vector<int> ans(q);
-  auto tree{RSQ_RUQ<int>(n)};
+  BIT<int> tree{n};
   vector<int> memo(n, -1);
   for (auto r{0}; r < n; ++r)
   {
@@ -655,7 +703,7 @@ int main()
     tree.update(memo[color], 1);
     for (auto [id, l] : v[r])
     {
-      ans[id] = tree.query(l, r + 1);
+      ans[id] = tree.sum(l, r + 1);
     }
   }
   for (auto e : ans)
